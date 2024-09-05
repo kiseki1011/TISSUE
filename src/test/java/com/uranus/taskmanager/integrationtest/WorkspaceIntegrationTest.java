4baeb096c2bd067bfb78bd65fd315b024a9ab62c
seungki1011 <kimseungki1011@gmail.com>
2024-09-05 13:00:32 +0900

    Test: 워크스페이스 생성에 대한 통합 테스트 리팩토링
    - 패키지 구조 변경
    - 테스트 추가

--- src/test/java/com/uranus/taskmanager/integrationtest/WorkspaceIntegrationTest.java
+++ src/test/java/com/uranus/taskmanager/integrationtest/WorkspaceIntegrationTest.java
@@ -1,13 +1,16 @@
-package com.uranus.taskmanager.api.controller;
+package com.uranus.taskmanager.integrationtest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uranus.taskmanager.api.repository.WorkspaceRepository;
 import com.uranus.taskmanager.api.request.WorkspaceCreateRequest;
 import com.uranus.taskmanager.api.service.WorkspaceService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class WorkspaceControllerIntegrationTest {
+class WorkspaceIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -26,10 +29,17 @@ class WorkspaceControllerIntegrationTest {
     private ObjectMapper objectMapper;
     @Autowired
     private WorkspaceService workspaceService;
+    @Autowired
+    private WorkspaceRepository workspaceRepository;
+
+    @BeforeEach
+    void setup() {
+        workspaceRepository.deleteAll();
+    }
 
     @Test
-    @DisplayName("name과 description 필드가 제공한 값으로 나오는지 검증한다")
-    public void 테스트3() throws Exception {
+    @DisplayName("POST /workspaces: 응답의 필드가 제공한 값과 일치하고, workspaceCode는 존재해야한다.")
+    public void 테스트1() throws Exception {
 
         WorkspaceCreateRequest request = WorkspaceCreateRequest.builder()
                 .name("Test Workspace")
@@ -46,6 +56,41 @@ class WorkspaceControllerIntegrationTest {
                 .andExpect(jsonPath("$.description").value("Test Description"))
                 .andExpect(jsonPath("$.workspaceCode").exists())
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("POST /workspaces: DB에 하나의 값만 저장된다.")
+    public void 테스트2() throws Exception {
+
+        WorkspaceCreateRequest request = WorkspaceCreateRequest.builder()
+                .name("Test Workspace")
+                .description("Test Description")
+                .build();
+
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/api/v1/workspaces")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isCreated())
+                .andDo(print());
+
+        assertThat(workspaceRepository.count()).isEqualTo(1L);
+    }
+
+    @Test
+    public void 테스트3() {
 
     }
+
+    @Test
+    public void 테스트4() {
+
+    }
+
+    @Test
+    public void 테스트5() {
+
+    }
+
 }
