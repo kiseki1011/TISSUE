75552807bf335d96d8a5ab788a37cbf5fd78bca0
seungki1011 <kimseungki1011@gmail.com>
2024-09-14 22:44:01 +0900

    Feat: Workspace, Member 관련 엔티티 수정/추가
    - User 엔티티를 Member 엔티티로 이름 변경(User가 예약어라서)
    - WorkspaceMember라는 중간 엔티티 추가
    - Member, Workspace 엔티티 수정

--- src/main/java/com/uranus/taskmanager/api/domain/user/Member.java
+++ src/main/java/com/uranus/taskmanager/api/domain/user/Member.java
@@ -1,4 +1,4 @@
-package com.uranus.taskmanager.api.domain.workspace;
+package com.uranus.taskmanager.api.domain.user;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,37 +19,32 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Workspace {
-
+public class Member {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "WORKSPACE_ID")
-	private Long id;
+	@Column(name = "MEMBER_ID")
+	Long id;
 
-	@Column(unique = true) //  nullable = false 사용 고려
-	private String workspaceCode;
+	@Column(unique = true, nullable = false)
+	private String username;
+
+	@Column(unique = true, nullable = false)
+	private String email;
 
 	@Column(nullable = false)
-	private String name;
+	private String password;
 
-	private String description;
-
-	@OneToMany(mappedBy = "workspace")
+	@OneToMany(mappedBy = "member")
 	private List<WorkspaceMember> workspaceMembers = new ArrayList<>();
 
 	/**
 	 * @param id - 빌더에 id 필드 제외를 고려하자(테스트를 위해 리플렉션을 사용)
 	 */
 	@Builder
-	public Workspace(Long id, String workspaceCode, String name, String description) {
+	public Member(Long id, String username, String email, String password) {
 		this.id = id;
-		this.workspaceCode = workspaceCode;
-		this.name = name;
-		this.description = description;
+		this.username = username;
+		this.email = email;
+		this.password = password;
 	}
-
-	public void setWorkspaceCode(String workspaceCode) {
-		this.workspaceCode = workspaceCode;
-	}
-
 }
---
+++ src/main/java/com/uranus/taskmanager/api/domain/workspaceuser/WorkspaceMember.java
@@ -0,0 +1,59 @@
+package com.uranus.taskmanager.api.domain.workspaceuser;
+
+import com.uranus.taskmanager.api.domain.user.Member;
+import com.uranus.taskmanager.api.domain.workspace.Workspace;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WorkspaceMember {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "MEMBER_ID", nullable = false)
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "WORKSPACE_ID", nullable = false)
+	private Workspace workspace;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private WorkspaceRole role;
+
+	private String nickname;
+
+	@Builder
+	public WorkspaceMember(Member member, Workspace workspace) {
+		this.member = member;
+		this.workspace = workspace;
+	}
+
+	public static WorkspaceMember createWorkspaceMember(Member member, Workspace workspace) {
+		WorkspaceMember workspaceMember = WorkspaceMember.builder()
+			.member(member)
+			.workspace(workspace)
+			.build();
+		member.getWorkspaceMembers().add(workspaceMember);
+		workspace.getWorkspaceMembers().add(workspaceMember);
+		return workspaceMember;
+	}
+}
