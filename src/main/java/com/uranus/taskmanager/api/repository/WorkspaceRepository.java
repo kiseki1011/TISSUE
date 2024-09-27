a4a1b34dd580dcc2669ccaf36af707860da9ee37
seungki1011 <kimseungki1011@gmail.com>
2024-09-27 23:59:19 +0900

    Feat: 워크 스페이스 코드 생성 로직 수정 및 추가
    - 워크스페이스 생성에 대한 서비스를 기존 서비스에서 분리
    - WorkspaceCreateService라는 인터페이스를 의존하도록 구현
    - WorkspaceCreateService의 구현체로 HandleDatabaseExceptionService추가
    - 해당 구현체는 워크스페이스 코드의 중복으로 발생하는 DB예외를 서비스 계층에서 잡아서 핸들링
    - ConstraintViolation을 서비스에서 잡아서 워크스페이스 코드 재생성하는 서비스임
    - 만약 중복이 발생할 확률이 매우 적으면 해당 방법이 고려될 수 있음
    - 이때 UnexpectedRollbackException이 발생해 해결하기 위해서 레포지토리의 save()에 REQUIRES_NEW를 사용해야 함
    - 데이터 JPA에서 이를 구현하기 위해서는 커스텀 레포지토리를 사용해야 함
    - 그렇기 때문에 커스텀 레포지토리 추가 후 saveWithNewTransaction()이라는 메서드에 REQUIRES_NEW를 적용 함
    
    Close #32

--- src/main/java/com/uranus/taskmanager/api/repository/WorkspaceRepository.java
+++ src/main/java/com/uranus/taskmanager/api/repository/WorkspaceRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.uranus.taskmanager.api.domain.workspace.Workspace;
 
-public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
+public interface WorkspaceRepository extends JpaRepository<Workspace, Long>, CustomWorkspaceRepository {
 
 	Optional<Workspace> findByWorkspaceCode(String workspaceCode);
+
+	boolean existsByWorkspaceCode(String workspaceCode);
 }
