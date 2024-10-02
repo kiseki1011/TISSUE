bbd0f62fca10d6c747d01636d90223cdf722fe88
seungki1011 <kimseungki1011@gmail.com>
2024-10-03 05:54:37 +0900

    Feat: 워크스페이스 내의 권한 검증 로직 구현
    - 커스텀 애노테이션 RoleRequired 추가
    - AuthorizationInterceptor 추가
    - 인터셉터를 통해 @RoleRequired가 붙은 API에서 로그인된 사용자의 권한 검증
    - WebMvcConfig에 인터셉터 등록
    
    - #38

--- src/main/java/com/uranus/taskmanager/api/global/config/WebMvcConfig.java
+++ src/main/java/com/uranus/taskmanager/api/global/config/WebMvcConfig.java
@@ -9,18 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.uranus.taskmanager.api.auth.AuthenticationInterceptor;
 import com.uranus.taskmanager.api.auth.LoginMemberArgumentResolver;
+import com.uranus.taskmanager.api.workspacemember.authorization.AuthorizationInterceptor;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+	private final AuthorizationInterceptor authorizationInterceptor;
 	private final AuthenticationInterceptor authenticationInterceptor;
 	private final LoginMemberArgumentResolver loginMemberArgumentResolver;
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(authenticationInterceptor);
+		registry.addInterceptor(authorizationInterceptor);
 	}
 
 	@Override
