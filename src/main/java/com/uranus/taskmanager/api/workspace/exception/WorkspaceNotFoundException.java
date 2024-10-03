24aee9e260da832efe29c705800a6840ab44ab29
seungki1011 <kimseungki1011@gmail.com>
2024-10-04 07:12:49 +0900

    Style: 예외 메세지 수정

--- src/main/java/com/uranus/taskmanager/api/workspace/exception/WorkspaceNotFoundException.java
+++ src/main/java/com/uranus/taskmanager/api/workspace/exception/WorkspaceNotFoundException.java
@@ -2,11 +2,9 @@ package com.uranus.taskmanager.api.workspace.exception;
 
 import org.springframework.http.HttpStatus;
 
-import com.uranus.taskmanager.api.workspace.exception.WorkspaceException;
-
 public class WorkspaceNotFoundException extends WorkspaceException {
 
-	private static final String MESSAGE = "Workspace was not found for given code.";
+	private static final String MESSAGE = "Workspace was not found for the given code";
 	private static final HttpStatus HTTP_STATUS = HttpStatus.NOT_FOUND;
 
 	public WorkspaceNotFoundException() {
