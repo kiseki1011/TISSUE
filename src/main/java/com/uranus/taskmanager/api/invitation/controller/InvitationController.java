035a91a018aa88c0476a60238195b71d8b2fefe5
seungki1011 <kimseungki1011@gmail.com>
2024-10-11 08:11:33 +0900

    Feat: 초대 수락 기능 구현
    - InvitationAcceptResponse 추가
    - Invitation 컨트롤러, 서비스, 레포지토리 추가
    - 초대 수락의 동작 과정은 다음과 같음
      - 현재 로그인 정보와 경로 변수로 전달한 워크스페이스 코드를 사용해서 초대 조회
      - 초대가 PENDING 상태가 아니라면 예외 발생(PENDING이 아니라는 뜻은 이미 처리되었다는 의미)
      - 초대 수락 진행(초대의 상태를 ACCEPTED로 변경)
      - 초대 저장
      - 초대를 수락한 멤버를 워크스페이스에 참여 시키는 로직
      - WorkspaceMember 생성과 저장

---
+++ src/main/java/com/uranus/taskmanager/api/invitation/controller/InvitationController.java
@@ -0,0 +1,39 @@
+package com.uranus.taskmanager.api.invitation.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.uranus.taskmanager.api.auth.LoginMember;
+import com.uranus.taskmanager.api.auth.LoginRequired;
+import com.uranus.taskmanager.api.auth.dto.request.LoginMemberDto;
+import com.uranus.taskmanager.api.common.ApiResponse;
+import com.uranus.taskmanager.api.invitation.dto.response.InvitationAcceptResponse;
+import com.uranus.taskmanager.api.invitation.service.InvitationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/invitations")
+public class InvitationController {
+
+	private final InvitationService invitationService;
+
+	@LoginRequired
+	@PostMapping("/{workspaceCode}/accept")
+	public ApiResponse<InvitationAcceptResponse> acceptInvitation(@PathVariable String workspaceCode,
+		@LoginMember LoginMemberDto loginMember) {
+
+		InvitationAcceptResponse response = invitationService.acceptInvitation(loginMember, workspaceCode);
+		return ApiResponse.ok("Invitation Accepted", response);
+	}
+
+	@LoginRequired
+	@PostMapping("/{workspaceCode}/reject")
+	public ApiResponse<Void> rejectInvitation() {
+
+		return ApiResponse.ok("Invitation Rejected", null);
+	}
+}
