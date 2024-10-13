d08634e6e5c895517df5cee352cbcd1f9a5ac6e9
seungki1011 <kimseungki1011@gmail.com>
2024-10-13 22:55:02 +0900

    Test: 초대 수락에 대한 테스트 추가

---
+++ src/test/java/com/uranus/taskmanager/api/invitation/controller/InvitationControllerTest.java
@@ -0,0 +1,132 @@
+package com.uranus.taskmanager.api.invitation.controller;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uranus.taskmanager.api.auth.SessionKey;
+import com.uranus.taskmanager.api.auth.dto.request.LoginMemberDto;
+import com.uranus.taskmanager.api.global.config.WebMvcConfig;
+import com.uranus.taskmanager.api.invitation.InvitationStatus;
+import com.uranus.taskmanager.api.invitation.domain.Invitation;
+import com.uranus.taskmanager.api.invitation.dto.response.InvitationAcceptResponse;
+import com.uranus.taskmanager.api.invitation.exception.InvitationNotFoundException;
+import com.uranus.taskmanager.api.invitation.service.InvitationService;
+import com.uranus.taskmanager.api.member.domain.Member;
+import com.uranus.taskmanager.api.member.repository.MemberRepository;
+import com.uranus.taskmanager.api.workspace.domain.Workspace;
+import com.uranus.taskmanager.api.workspace.repository.WorkspaceRepository;
+import com.uranus.taskmanager.api.workspace.service.CheckCodeDuplicationService;
+import com.uranus.taskmanager.api.workspace.service.WorkspaceService;
+import com.uranus.taskmanager.api.workspacemember.WorkspaceRole;
+import com.uranus.taskmanager.api.workspacemember.domain.WorkspaceMember;
+import com.uranus.taskmanager.api.workspacemember.repository.WorkspaceMemberRepository;
+import com.uranus.taskmanager.fixture.TestFixture;
+
+@WebMvcTest(controllers = InvitationController.class)
+class InvitationControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@MockBean
+	private WorkspaceService workspaceService;
+	@MockBean
+	private CheckCodeDuplicationService workspaceCreateService;
+	@MockBean
+	private MemberRepository memberRepository;
+	@MockBean
+	private WorkspaceRepository workspaceRepository;
+	@MockBean
+	private WorkspaceMemberRepository workspaceMemberRepository;
+	@MockBean
+	private InvitationService invitationService;
+	@MockBean
+	private WebMvcConfig webMvcConfig;
+
+	TestFixture testFixture;
+
+	@BeforeEach
+	public void setup() {
+		testFixture = new TestFixture();
+	}
+
+	@Test
+	@DisplayName("유효한 코드로 초대를 수락하면 200OK를 응답받고 초대 수락 응답 DTO를 데이터로 받는다")
+	void test1() throws Exception {
+		// given
+		String workspaceCode = "testcode";
+		String loginId = "user123";
+		String email = "user123@test.com";
+
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute(SessionKey.LOGIN_MEMBER, loginId);
+
+		Workspace workspace = testFixture.createWorkspace(workspaceCode);
+		Member member = testFixture.createMember(loginId, email);
+
+		Invitation invitation = Invitation.builder()
+			.status(InvitationStatus.PENDING)
+			.workspace(workspace)
+			.member(member)
+			.build();
+
+		InvitationAcceptResponse acceptResponse = InvitationAcceptResponse.fromEntity(invitation,
+			WorkspaceMember.addWorkspaceMember(member, workspace, WorkspaceRole.USER,
+				member.getEmail()));
+
+		// when
+		when(invitationService.acceptInvitation(any(LoginMemberDto.class), eq(workspaceCode)))
+			.thenReturn(acceptResponse);
+
+		// then
+		mockMvc.perform(post("/api/v1/invitations/{workspaceCode}/accept", workspaceCode)
+				.session(session)
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.message").value("Invitation Accepted"))
+			.andExpect(jsonPath("$.data").exists())
+			.andDo(print());
+	}
+
+	@Test
+	@DisplayName("유효하지 않은 코드로 초대를 수락하면 예외가 발생한다")
+	void test2() throws Exception {
+		// given
+		String workspaceCode = "invalidcode";
+		String loginId = "user123";
+		String email = "user123@test.com";
+
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute(SessionKey.LOGIN_MEMBER, loginId);
+
+		// when
+		when(invitationService.acceptInvitation(any(LoginMemberDto.class), eq(workspaceCode)))
+			.thenThrow(new InvitationNotFoundException());
+
+		// then
+		mockMvc.perform(post("/api/v1/invitations/{workspaceCode}/accept", workspaceCode)
+				.session(session)
+				.contentType(MediaType.APPLICATION_JSON))
+			// Todo: 추후 ResponseEntity를 적용하면 응답 상태 헤더는 해당 예외에서 상태를 꺼내서 사용한다 (400 -> 404로 변경)
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.message").value("Invitation was not found for the given code"))
+			.andDo(print());
+	}
+
+}
---
+++ src/test/java/com/uranus/taskmanager/api/invitation/service/InvitationServiceTest.java
@@ -0,0 +1,180 @@
+package com.uranus.taskmanager.api.invitation.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.uranus.taskmanager.api.auth.dto.request.LoginMemberDto;
+import com.uranus.taskmanager.api.invitation.InvitationStatus;
+import com.uranus.taskmanager.api.invitation.domain.Invitation;
+import com.uranus.taskmanager.api.invitation.dto.response.InvitationAcceptResponse;
+import com.uranus.taskmanager.api.invitation.exception.InvalidInvitationStatusException;
+import com.uranus.taskmanager.api.invitation.exception.InvitationNotFoundException;
+import com.uranus.taskmanager.api.invitation.repository.InvitationRepository;
+import com.uranus.taskmanager.api.member.domain.Member;
+import com.uranus.taskmanager.api.member.repository.MemberRepository;
+import com.uranus.taskmanager.api.workspace.domain.Workspace;
+import com.uranus.taskmanager.api.workspace.repository.WorkspaceRepository;
+import com.uranus.taskmanager.api.workspacemember.domain.WorkspaceMember;
+import com.uranus.taskmanager.api.workspacemember.repository.WorkspaceMemberRepository;
+import com.uranus.taskmanager.fixture.TestFixture;
+
+@ExtendWith(MockitoExtension.class)
+class InvitationServiceTest {
+
+	@InjectMocks
+	private InvitationService invitationService;
+
+	@Mock
+	private WorkspaceRepository workspaceRepository;
+	@Mock
+	private MemberRepository memberRepository;
+	@Mock
+	private WorkspaceMemberRepository workspaceMemberRepository;
+	@Mock
+	private InvitationRepository invitationRepository;
+
+	TestFixture testFixture;
+
+	@BeforeEach
+	public void setup() {
+		testFixture = new TestFixture();
+	}
+
+	@Test
+	@DisplayName("유효한 로그인 정보와 코드를 사용해서 초대를 수락하면 초대가 저장된다")
+	void test1() {
+		// given
+		String workspaceCode = "testcode";
+		String loginId = "user123";
+		String email = "user123@test.com";
+
+		Workspace workspace = testFixture.createWorkspace(workspaceCode);
+		Member member = testFixture.createMember(loginId, email);
+		WorkspaceMember workspaceMember = testFixture.createUserWorkspaceMember(member, workspace);
+		LoginMemberDto loginMember = testFixture.createLoginMemberDto(loginId, email);
+		Invitation invitation = testFixture.createPendingInvitation(workspace, member);
+
+		when(invitationRepository.findByWorkspaceCodeAndMemberLoginId(workspaceCode, loginId)).thenReturn(
+			Optional.of(invitation));
+		when(invitationRepository.save(eq(invitation))).thenReturn(invitation);
+		when(workspaceMemberRepository.save(any(WorkspaceMember.class))).thenReturn(workspaceMember);
+
+		// when
+		InvitationAcceptResponse acceptResponse = invitationService.acceptInvitation(loginMember, workspaceCode);
+
+		// then
+		assertThat(acceptResponse.getWorkspaceCode()).isEqualTo(workspaceCode);
+		verify(invitationRepository, times(1)).save(eq(invitation));
+
+	}
+
+	@Test
+	@DisplayName("초대가 성공하면 초대의 상태가 수락으로 변경된다")
+	void test2() {
+		// given
+		String workspaceCode = "testcode";
+		String loginId = "user123";
+		String email = "user123@test.com";
+
+		Workspace workspace = testFixture.createWorkspace(workspaceCode);
+		Member member = testFixture.createMember(loginId, email);
+		WorkspaceMember workspaceMember = testFixture.createUserWorkspaceMember(member, workspace);
+		LoginMemberDto loginMember = testFixture.createLoginMemberDto(loginId, email);
+		Invitation invitation = testFixture.createPendingInvitation(workspace, member);
+
+		when(invitationRepository.findByWorkspaceCodeAndMemberLoginId(workspaceCode, loginId)).thenReturn(
+			Optional.of(invitation));
+		when(invitationRepository.save(eq(invitation))).thenReturn(invitation);
+		when(workspaceMemberRepository.save(any(WorkspaceMember.class))).thenReturn(workspaceMember);
+
+		// when
+		invitationService.acceptInvitation(loginMember, workspaceCode);
+		Optional<Invitation> acceptedInvitation = invitationRepository.findByWorkspaceCodeAndMemberLoginId(
+			workspaceCode, loginId);
+
+		// then
+		assertThat(acceptedInvitation).isPresent();
+		assertThat(acceptedInvitation.get().getStatus()).isEqualTo(InvitationStatus.ACCEPTED);
+	}
+
+	@Test
+	@DisplayName("초대의 수락이 성공하면 해당 워크스페이스에 참여 된다")
+	void test3() {
+		// given
+		String workspaceCode = "testcode";
+		String loginId = "user123";
+		String email = "user123@test.com";
+
+		Workspace workspace = testFixture.createWorkspace(workspaceCode);
+		Member member = testFixture.createMember(loginId, email);
+		WorkspaceMember workspaceMember = testFixture.createUserWorkspaceMember(member, workspace);
+		LoginMemberDto loginMember = testFixture.createLoginMemberDto(loginId, email);
+		Invitation invitation = testFixture.createPendingInvitation(workspace, member);
+
+		when(invitationRepository.findByWorkspaceCodeAndMemberLoginId(workspaceCode, loginId)).thenReturn(
+			Optional.of(invitation));
+		when(invitationRepository.save(eq(invitation))).thenReturn(invitation);
+		when(workspaceMemberRepository.save(any(WorkspaceMember.class))).thenReturn(workspaceMember);
+
+		// when
+		InvitationAcceptResponse acceptResponse = invitationService.acceptInvitation(loginMember, workspaceCode);
+
+		// then
+		assertThat(acceptResponse.getWorkspaceCode()).isEqualTo(workspaceCode);
+		verify(workspaceMemberRepository, times(1)).save(any(WorkspaceMember.class));
+	}
+
+	@Test
+	@DisplayName("유효하지 않은 코드를 사용해서 초대를 수락하면 예외가 발생한다")
+	void test4() {
+		// given
+		String workspaceCode = "invalidcode";
+		String loginId = "user123";
+		String email = "user123@test.com";
+
+		Workspace workspace = testFixture.createWorkspace(workspaceCode);
+		Member member = testFixture.createMember(loginId, email);
+		LoginMemberDto loginMember = testFixture.createLoginMemberDto(loginId, email);
+
+		when(invitationRepository.findByWorkspaceCodeAndMemberLoginId(workspaceCode, loginId)).thenReturn(
+			Optional.empty());
+
+		// when & then
+		assertThatThrownBy(() -> invitationService.acceptInvitation(loginMember, workspaceCode)).isInstanceOf(
+			InvitationNotFoundException.class);
+
+	}
+
+	@Test
+	@DisplayName("존재하는 초대의 상태가 PENDING이 아니면 예외가 발생한다")
+	void test5() {
+		// given
+		String workspaceCode = "invalidcode";
+		String loginId = "user123";
+		String email = "user123@test.com";
+
+		Workspace workspace = testFixture.createWorkspace(workspaceCode);
+		Member member = testFixture.createMember(loginId, email);
+		LoginMemberDto loginMember = testFixture.createLoginMemberDto(loginId, email);
+		Invitation invitation = testFixture.createAcceptedInvitation(workspace, member);
+
+		when(invitationRepository.findByWorkspaceCodeAndMemberLoginId(workspaceCode, loginId))
+			.thenReturn(Optional.of(invitation));
+
+		// when & then
+		assertThatThrownBy(() -> invitationService.acceptInvitation(loginMember, workspaceCode)).isInstanceOf(
+			InvalidInvitationStatusException.class);
+
+	}
+
+}
