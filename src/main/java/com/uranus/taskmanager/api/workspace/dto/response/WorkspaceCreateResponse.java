c4796e1c324953f297e8d967936077c238b9fd91
seungki1011 <kimseungki1011@gmail.com>
2024-10-21 01:01:22 +0900

    Refactor: 워크스페이스 엔티티, DTO 리팩토링
    - 주석 추가, 제거
    - 기본 생성자 @NoArgsConstructor 사용

--- src/main/java/com/uranus/taskmanager/api/workspace/dto/response/WorkspaceCreateResponse.java
+++ src/main/java/com/uranus/taskmanager/api/workspace/dto/response/WorkspaceCreateResponse.java
@@ -4,16 +4,20 @@ import com.uranus.taskmanager.api.workspace.domain.Workspace;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
-@ToString
-@Builder
 public class WorkspaceCreateResponse {
 
 	private final String code;
 	private final String name;
 	private final String description;
+
+	@Builder
+	public WorkspaceCreateResponse(String code, String name, String description) {
+		this.code = code;
+		this.name = name;
+		this.description = description;
+	}
 
 	public static WorkspaceCreateResponse from(Workspace workspace) {
 		return WorkspaceCreateResponse.builder()
