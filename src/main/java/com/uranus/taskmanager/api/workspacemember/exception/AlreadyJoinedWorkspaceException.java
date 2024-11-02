22917903d1384fe9c0745b88b361bde35936f5a4
seungki1011 <kimseungki1011@gmail.com>
2024-11-03 01:36:21 +0900

    Refactor: MemberAlreadyParticipating -> AlreadyJoined 예외 이름 변경
    - 예외 이름을 변경
    - 예외 메세지 수정

--- src/main/java/com/uranus/taskmanager/api/workspacemember/exception/AlreadyJoinedWorkspaceException.java
+++ src/main/java/com/uranus/taskmanager/api/workspacemember/exception/AlreadyJoinedWorkspaceException.java
@@ -4,15 +4,15 @@ import org.springframework.http.HttpStatus;
 
 import com.uranus.taskmanager.api.common.exception.WorkspaceMemberException;
 
-public class MemberAlreadyParticipatingException extends WorkspaceMemberException {
-	private static final String MESSAGE = "Current login member is already participating in this Workspace";
+public class AlreadyJoinedWorkspaceException extends WorkspaceMemberException {
+	private static final String MESSAGE = "Member already joined this Workspace";
 	private static final HttpStatus HTTP_STATUS = HttpStatus.BAD_REQUEST;
 
-	public MemberAlreadyParticipatingException() {
+	public AlreadyJoinedWorkspaceException() {
 		super(MESSAGE, HTTP_STATUS);
 	}
 
-	public MemberAlreadyParticipatingException(Throwable cause) {
+	public AlreadyJoinedWorkspaceException(Throwable cause) {
 		super(MESSAGE, HTTP_STATUS, cause);
 	}
 }
