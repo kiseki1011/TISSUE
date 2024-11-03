316f1000b4a4d03ae49b7c9cd91872dbe02a6ec1
seungki1011 <kimseungki1011@gmail.com>
2024-11-03 19:49:33 +0900

    Fix: 이름 오타 수정

--- src/test/java/com/uranus/taskmanager/fixture/repository/MemberRepositoryFixture.java
+++ src/test/java/com/uranus/taskmanager/fixture/repository/MemberRepositoryFixture.java
@@ -6,27 +6,30 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.uranus.taskmanager.api.member.domain.Member;
 import com.uranus.taskmanager.api.member.repository.MemberRepository;
+import com.uranus.taskmanager.api.security.PasswordEncoder;
 
 @Component
 @Transactional
-public class MemberRespositoryFixture {
+public class MemberRepositoryFixture {
 
 	@Autowired
 	private MemberRepository memberRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	/**
 	 * 멤버를 생성하고 저장합니다.
 	 *
 	 * @param loginId - 로그인 ID
 	 * @param email - 이메일
-	 * @param password - 비밀번호 (암호화되지 않은 상태)
+	 * @param password - 비밀번호 (암호화)
 	 * @return 저장된 Member 객체
 	 */
 	public Member createMember(String loginId, String email, String password) {
 		Member member = Member.builder()
 			.loginId(loginId)
 			.email(email)
-			.password(password) // 테스트에서는 비밀번호를 미리 암호화하지 않음
+			.password(passwordEncoder.encode(password))
 			.build();
 		return memberRepository.save(member);
 	}
