653622c8dca6b0012d2136d95b5217ac4c78a5d5
seungki1011 <kimseungki1011@gmail.com>
2024-11-05 14:53:06 +0900

    Feat: 로그인 응답에 id 필드 추가
    - 세션에 id를 저장하기 위해서는 id를 받아야 함
    - Todo에 고민 적음. 확인 요망.

--- src/main/java/com/uranus/taskmanager/api/authentication/dto/response/LoginResponse.java
+++ src/main/java/com/uranus/taskmanager/api/authentication/dto/response/LoginResponse.java
@@ -2,21 +2,37 @@ package com.uranus.taskmanager.api.authentication.dto.response;
 
 import com.uranus.taskmanager.api.member.domain.Member;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+/**
+ * Todo
+ *  - 다음의 선택들을 고려 중
+ *  - LoginResponse -> MemberSession 이름 변경, 그대로 사용
+ *  - LoginResponse의 내용물을 MemberSession으로 포장하고 사용
+ *  - 그냥 지금 처럼 LoginResponse에서 바로 꺼내서 세션에 저장
+ */
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LoginResponse {
+	private Long id;
+	private String loginId;
+	private String email;
 
-	private final String loginId;
-	private final String email;
+	@Builder
+	public LoginResponse(Long id, String loginId, String email) {
+		this.id = id;
+		this.loginId = loginId;
+		this.email = email;
+	}
 
 	public static LoginResponse from(Member member) {
 		return LoginResponse.builder()
+			.id(member.getId())
 			.loginId(member.getLoginId())
 			.email(member.getEmail())
 			.build();
 	}
-
 }
