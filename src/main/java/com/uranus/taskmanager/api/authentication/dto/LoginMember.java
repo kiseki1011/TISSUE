6d55583a7e0fa1b6a856a993bce2adb55321858f
seungki1011 <kimseungki1011@gmail.com>
2024-11-05 15:22:16 +0900

    Feat: LoginMember에 id 필드 추가
    - 서비스 계층에서 기존에 loginId로 Member를 찾던것을 id로 찾는 것으로 변경하기 위해

--- src/main/java/com/uranus/taskmanager/api/authentication/dto/LoginMember.java
+++ src/main/java/com/uranus/taskmanager/api/authentication/dto/LoginMember.java
@@ -7,18 +7,20 @@ import lombok.Getter;
 
 @Getter
 public class LoginMember {
-
+	private final Long id;
 	private final String loginId;
 	private final String email;
 
 	@Builder
-	public LoginMember(String loginId, String email) {
+	public LoginMember(Long id, String loginId, String email) {
+		this.id = id;
 		this.loginId = loginId;
 		this.email = email;
 	}
 
 	public static LoginMember from(Member member) {
 		return LoginMember.builder()
+			.id(member.getId())
 			.loginId(member.getLoginId())
 			.email(member.getEmail())
 			.build();
