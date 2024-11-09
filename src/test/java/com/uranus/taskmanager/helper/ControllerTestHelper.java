060bcda8fc57dc906e8a625cf2ff6ee363eb8ed6
seungki1011 <kimseungki1011@gmail.com>
2024-11-09 19:14:13 +0900

    Feat: ControllerTestHelper에 WebMvcTestConfig 적용
    - @Import로 적용

--- src/test/java/com/uranus/taskmanager/helper/ControllerTestHelper.java
+++ src/test/java/com/uranus/taskmanager/helper/ControllerTestHelper.java
@@ -3,11 +3,15 @@ package com.uranus.taskmanager.helper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uranus.taskmanager.api.authentication.controller.AuthenticationController;
 import com.uranus.taskmanager.api.authentication.service.AuthenticationService;
+import com.uranus.taskmanager.api.global.config.WebMvcConfig;
 import com.uranus.taskmanager.api.invitation.controller.InvitationController;
 import com.uranus.taskmanager.api.invitation.repository.InvitationRepository;
 import com.uranus.taskmanager.api.invitation.service.InvitationService;
@@ -22,17 +26,24 @@ import com.uranus.taskmanager.api.workspace.service.WorkspaceAccessService;
 import com.uranus.taskmanager.api.workspace.service.WorkspaceCommandService;
 import com.uranus.taskmanager.api.workspace.service.WorkspaceQueryService;
 import com.uranus.taskmanager.api.workspacemember.repository.WorkspaceMemberRepository;
+import com.uranus.taskmanager.config.WebMvcTestConfig;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@WebMvcTest(controllers = {
-	AuthenticationController.class,
-	InvitationController.class,
-	WorkspaceController.class,
-	WorkspaceAccessController.class,
-	MemberController.class
-})
+@WebMvcTest(
+	controllers = {
+		AuthenticationController.class,
+		InvitationController.class,
+		WorkspaceController.class,
+		WorkspaceAccessController.class,
+		MemberController.class
+	},
+	excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebMvcConfig.class)
+	}
+)
+@Import(value = WebMvcTestConfig.class)
 public abstract class ControllerTestHelper {
 	@Autowired
 	protected MockMvc mockMvc;
@@ -62,5 +73,5 @@ public abstract class ControllerTestHelper {
 	protected WorkspaceMemberRepository workspaceMemberRepository;
 	@MockBean
 	protected InvitationRepository invitationRepository;
-	
+
 }
