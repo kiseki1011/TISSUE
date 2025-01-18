027a52af117bf8ef100a55be5020065167df9466
seungki1011 <kimseungki1011@gmail.com>
2025-01-19 00:35:26 +0900

    Refactor: 로그인 시 회원 조회는 identifier로
    - 기존에는 loginId, email 필드 둘다 열어 놓고 요청을 받음
    - 지금은 identifier 필드 하나 열어놓고 loginId 또는 email 넣기 가능
    - 레포지토리에도 관련 조회 메서드 추가

--- backend/src/main/java/com/tissue/api/security/authentication/service/AuthenticationService.java
+++ backend/src/main/java/com/tissue/api/security/authentication/service/AuthenticationService.java
@@ -3,10 +3,10 @@ package com.tissue.api.security.authentication.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tissue.api.common.exception.InvalidCredentialsException;
 import com.tissue.api.member.domain.Member;
 import com.tissue.api.member.domain.repository.MemberRepository;
 import com.tissue.api.member.validator.MemberValidator;
-import com.tissue.api.security.authentication.exception.InvalidLoginIdentityException;
 import com.tissue.api.security.authentication.presentation.dto.request.LoginRequest;
 import com.tissue.api.security.authentication.presentation.dto.response.LoginResponse;
 
@@ -22,15 +22,18 @@ public class AuthenticationService {
 	@Transactional
 	public LoginResponse login(LoginRequest request) {
 
-		Member member = findMemberByLoginIdOrEmail(request);
+		Member member = findMember(request.identifier());
 
-		memberValidator.validatePasswordMatch(request.getPassword(), member.getPassword());
+		memberValidator.validatePasswordMatch(request.password(), member.getPassword());
 
 		return LoginResponse.from(member);
 	}
 
-	private Member findMemberByLoginIdOrEmail(LoginRequest request) {
-		return memberRepository.findByLoginIdOrEmail(request.getLoginId(), request.getEmail())
-			.orElseThrow(InvalidLoginIdentityException::new);
+	private Member findMember(String identifier) {
+		return memberRepository.findByIdentifier(identifier)
+			.orElseThrow(() -> new InvalidCredentialsException(
+					String.format("Member not found with login ID or email. identifier: %s", identifier)
+				)
+			);
 	}
 }
