46555808c1bd2f42bd23e6d523faca76b7e4da9f
seungki1011 <kimseungki1011@gmail.com>
2025-01-19 00:37:38 +0900

    Refactor: AccessDeniedException -> ForbiddenOperationException
    - 이름 변경
    - AccessDeniedException은 이미 존재(헷갈릴 여지가 있음)

--- backend/src/main/java/com/tissue/api/common/exception/ForbiddenOperationException.java
+++ backend/src/main/java/com/tissue/api/common/exception/ForbiddenOperationException.java
@@ -2,8 +2,8 @@ package com.tissue.api.common.exception;
 
 import org.springframework.http.HttpStatus;
 
-public class AccessDeniedException extends TissueException {
-	public AccessDeniedException(String message) {
+public class ForbiddenOperationException extends TissueException {
+	public ForbiddenOperationException(String message) {
 		super(message, HttpStatus.FORBIDDEN);
 	}
 }
