dc5aaec479a77e2fc0995c58a60d311dca39c09c
seungki1011 <kimseungki1011@gmail.com>
2025-01-19 23:43:09 +0900

    Refactor: use UNAUTHORIZED for AuthenticationFailedException

--- backend/src/main/java/com/tissue/api/common/exception/type/AuthenticationFailedException.java
+++ backend/src/main/java/com/tissue/api/common/exception/type/AuthenticationFailedException.java
@@ -7,10 +7,10 @@ import com.tissue.api.common.exception.TissueException;
 public class AuthenticationFailedException extends TissueException {
 
 	public AuthenticationFailedException(String message) {
-		super(message, HttpStatus.BAD_REQUEST);
+		super(message, HttpStatus.UNAUTHORIZED);
 	}
 
 	public AuthenticationFailedException(String messageCode, Object... args) {
-		super(messageCode, args, HttpStatus.BAD_REQUEST);
+		super(messageCode, args, HttpStatus.UNAUTHORIZED);
 	}
 }
