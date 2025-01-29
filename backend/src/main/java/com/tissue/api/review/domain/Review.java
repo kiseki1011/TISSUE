2275cc2171357b5ee0f1267ef5a125445136b832
seungki1011 <kimseungki1011@gmail.com>
2025-01-30 01:37:57 +0900

    Refactor: add issueKey field to Review

--- backend/src/main/java/com/tissue/api/review/domain/Review.java
+++ backend/src/main/java/com/tissue/api/review/domain/Review.java
@@ -52,6 +52,9 @@ public class Review extends WorkspaceContextBaseEntity {
 	@Column(nullable = false)
 	private String workspaceCode;
 
+	@Column
+	private String issueKey;
+
 	@Builder
 	public Review(
 		IssueReviewer issueReviewer,
@@ -65,12 +68,21 @@ public class Review extends WorkspaceContextBaseEntity {
 		this.title = title;
 		this.content = content;
 		this.reviewRound = reviewRound;
-		this.workspaceCode = getWorkspaceCode();
+		this.workspaceCode = issueReviewer.getIssue().getWorkspaceCode();
+		this.issueKey = issueReviewer.getIssue().getIssueKey();
 	}
 
 	public void updateStatus(ReviewStatus status) {
 		validateCanUpdateStatus();
 		this.status = status;
+	}
+
+	public void updateTitle(String title) {
+		this.title = title;
+	}
+
+	public void updateContent(String content) {
+		this.content = content;
 	}
 
 	public void validateIsAuthor(Long workspaceMemberId) {
@@ -85,18 +97,6 @@ public class Review extends WorkspaceContextBaseEntity {
 				)
 			);
 		}
-	}
-
-	public void updateTitle(String title) {
-		this.title = title;
-	}
-
-	public void updateContent(String content) {
-		this.content = content;
-	}
-
-	public String getWorkspaceCode() {
-		return issueReviewer.getReviewer().getWorkspaceCode();
 	}
 
 	private void validateCanUpdateStatus() {
