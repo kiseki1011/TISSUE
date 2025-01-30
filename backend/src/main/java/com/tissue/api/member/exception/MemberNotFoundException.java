309cc4ea98eb309317e5628a817781d061089226
seungki1011 <kimseungki1011@gmail.com>
2025-01-30 23:47:00 +0900

    Refactor: add new message constant
    - identifier(식별자)로 조회할 때 찾지 못하는 경우에 대한 메세지 추가

--- backend/src/main/java/com/tissue/api/member/exception/MemberNotFoundException.java
+++ backend/src/main/java/com/tissue/api/member/exception/MemberNotFoundException.java
@@ -5,14 +5,13 @@ import com.tissue.api.common.exception.type.ResourceNotFoundException;
 public class MemberNotFoundException extends ResourceNotFoundException {
 
 	private static final String ID_MESSAGE = "Member not found with ID: %d";
-	private static final String EMAIL_MESSAGE = "Member not found with email: %s";
-	private static final String LOGIN_ID_MESSAGE = "Member not found with login ID: %s";
+	private static final String IDENTIFIER_MESSAGE = "Member not found with login ID or email. identifier: %s";
 
 	public MemberNotFoundException(Long id) {
 		super(String.format(ID_MESSAGE, id));
 	}
 
-	public MemberNotFoundException(String identifier, boolean isEmail) {
-		super(String.format(isEmail ? EMAIL_MESSAGE : LOGIN_ID_MESSAGE, identifier));
+	public MemberNotFoundException(String identifier) {
+		super(String.format(IDENTIFIER_MESSAGE, identifier));
 	}
 }
