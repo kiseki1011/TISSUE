dfa4c1662d53b8d7da7e746ebf64a13860d5aead
seungki1011 <kimseungki1011@gmail.com>
2025-01-31 15:12:36 +0900

    Refactor: reuse ReviewQueryService for Review search

--- backend/src/main/java/com/tissue/api/review/service/command/ReviewCommandService.java
+++ backend/src/main/java/com/tissue/api/review/service/command/ReviewCommandService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tissue.api.common.exception.type.ForbiddenOperationException;
-import com.tissue.api.common.exception.type.ResourceNotFoundException;
 import com.tissue.api.issue.domain.Issue;
 import com.tissue.api.issue.domain.enums.IssueStatus;
 import com.tissue.api.issue.service.query.IssueQueryService;
@@ -19,23 +18,23 @@ import com.tissue.api.review.presentation.dto.request.UpdateReviewStatusRequest;
 import com.tissue.api.review.presentation.dto.response.CreateReviewResponse;
 import com.tissue.api.review.presentation.dto.response.UpdateReviewResponse;
 import com.tissue.api.review.presentation.dto.response.UpdateReviewStatusResponse;
+import com.tissue.api.review.service.query.ReviewQueryService;
 import com.tissue.api.workspacemember.domain.WorkspaceMember;
 import com.tissue.api.workspacemember.domain.WorkspaceRole;
 import com.tissue.api.workspacemember.service.query.WorkspaceMemberQueryService;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * Todo
  *  - 알림 서비스 구현 필요
  *    - 리뷰 상태 변경 -> 모든 리뷰가 작성되었고 전부 APPROVED -> 이슈 assignees에게 알림
  */
-@Slf4j
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class ReviewCommandService {
 
+	private final ReviewQueryService reviewQueryService;
 	private final IssueQueryService issueQueryService;
 	private final WorkspaceMemberQueryService workspaceMemberQueryService;
 	private final ReviewRepository reviewRepository;
@@ -73,7 +72,7 @@ public class ReviewCommandService {
 		Long reviewerWorkspaceMemberId,
 		UpdateReviewRequest request
 	) {
-		Review review = findReview(reviewId);
+		Review review = reviewQueryService.findReview(reviewId);
 		review.validateIsAuthor(reviewerWorkspaceMemberId);
 
 		review.updateTitle(request.title());
@@ -93,7 +92,7 @@ public class ReviewCommandService {
 		Issue issue = issueQueryService.findIssue(issueKey, workspaceCode);
 
 		WorkspaceMember requester = workspaceMemberQueryService.findWorkspaceMember(requesterWorkspaceMemberId);
-		Review review = findReview(reviewId);
+		Review review = reviewQueryService.findReview(reviewId);
 
 		if (requester.roleIsLowerThan(WorkspaceRole.MANAGER)) {
 			review.validateIsAuthor(requesterWorkspaceMemberId);
@@ -107,15 +106,9 @@ public class ReviewCommandService {
 
 	private IssueReviewer findIssueReviewer(String issueKey, Long reviewerWorkspaceMemberId) {
 		return issueReviewerRepository.findByIssueKeyAndReviewerId(issueKey, reviewerWorkspaceMemberId)
-			.orElseThrow(() -> new ForbiddenOperationException(
-				String.format("Must be a reviewer to create a review. issueKey: %s, workspaceMemberId: %d",
-					issueKey, reviewerWorkspaceMemberId)));
-	}
-
-	private Review findReview(Long id) {
-		return reviewRepository.findById(id)
-			.orElseThrow(
-				() -> new ResourceNotFoundException(String.format("Review was not found with reviewId: %d", id)));
+			.orElseThrow(() -> new ForbiddenOperationException(String.format(
+				"Must be a reviewer to create a review. issueKey: %s, workspaceMemberId: %d",
+				issueKey, reviewerWorkspaceMemberId)));
 	}
 
 	private void updateIssueStatusBasedOnReviewStatus(Issue issue, ReviewStatus reviewStatus) {
