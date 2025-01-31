decb40524ca1c32d66de7f4d567e14eb32c3f700
seungki1011 <kimseungki1011@gmail.com>
2025-01-31 13:27:12 +0900

    Feat: add TeamQueryService

--- backend/src/main/java/com/tissue/api/team/service/query/TeamQueryService.java
+++ backend/src/main/java/com/tissue/api/team/service/query/TeamQueryService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tissue.api.common.exception.type.ResourceNotFoundException;
 import com.tissue.api.team.domain.Team;
 import com.tissue.api.team.domain.repository.TeamRepository;
 import com.tissue.api.team.presentation.dto.response.GetTeamsResponse;
@@ -23,5 +24,13 @@ public class TeamQueryService {
 		List<Team> teams = teamRepository.findAllByWorkspaceCodeOrderByCreatedDateAsc(workspaceCode);
 
 		return GetTeamsResponse.from(teams);
+	}
+
+	@Transactional(readOnly = true)
+	public Team findTeam(Long teamId, String code) {
+		return teamRepository.findByIdAndWorkspaceCode(teamId, code)
+			.orElseThrow(() -> new ResourceNotFoundException(String.format(
+				"Team was not found with teamId: %d, workspaceCode: %s",
+				teamId, code)));
 	}
 }
