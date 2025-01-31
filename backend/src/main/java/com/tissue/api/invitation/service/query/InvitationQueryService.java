b74b6db002f23b3254838ef7547dfa8f90c52edc
seungki1011 <kimseungki1011@gmail.com>
2025-01-31 13:23:17 +0900

    Feat: add InvitationQueryService

--- backend/src/main/java/com/tissue/api/invitation/service/query/InvitationQueryService.java
+++ backend/src/main/java/com/tissue/api/invitation/service/query/InvitationQueryService.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tissue.api.common.exception.type.ResourceNotFoundException;
+import com.tissue.api.invitation.domain.Invitation;
 import com.tissue.api.invitation.domain.repository.InvitationRepository;
 import com.tissue.api.invitation.presentation.dto.InvitationSearchCondition;
 import com.tissue.api.invitation.presentation.dto.response.InvitationResponse;
@@ -28,5 +30,12 @@ public class InvitationQueryService {
 			searchCondition.statuses(),
 			pageable
 		).map(InvitationResponse::from);
+	}
+
+	@Transactional(readOnly = true)
+	public Invitation findInvitation(Long invitationId) {
+		return invitationRepository.findById(invitationId)
+			.orElseThrow(() -> new ResourceNotFoundException(
+				String.format("Invitation not found with invitation id: %d", invitationId)));
 	}
 }
