089495ea02b4016e253c0670ec6a5011bc694ec6
seungki1011 <kimseungki1011@gmail.com>
2025-02-15 21:02:23 +0900

    Feat: add InvalidConfigurationException
    - 외부 설정값이 잘못된 경우의 커스텀 예외

---
+++ backend/src/main/java/com/tissue/api/common/exception/type/InvalidConfigurationException.java
@@ -0,0 +1,11 @@
+package com.tissue.api.common.exception.type;
+
+import org.springframework.http.HttpStatus;
+
+import com.tissue.api.common.exception.TissueException;
+
+public class InvalidConfigurationException extends TissueException {
+	public InvalidConfigurationException(String message) {
+		super(message, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
