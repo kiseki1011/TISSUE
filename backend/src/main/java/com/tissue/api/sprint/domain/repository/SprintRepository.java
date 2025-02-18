935031ce56d88a007b4a037e24cf641c4f22ff89
seungki1011 <kimseungki1011@gmail.com>
2025-02-18 13:10:53 +0900

    Feat: add method to SprintRepository
    - 스프린트키와 워크스페이스 코드를 통한 스프린트 조회 메서드 추가

--- backend/src/main/java/com/tissue/api/sprint/domain/repository/SprintRepository.java
+++ backend/src/main/java/com/tissue/api/sprint/domain/repository/SprintRepository.java
@@ -1,8 +1,12 @@
 package com.tissue.api.sprint.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tissue.api.sprint.domain.Sprint;
 
 public interface SprintRepository extends JpaRepository<Sprint, Long> {
+
+	Optional<Sprint> findBySprintKeyAndWorkspaceCode(String sprintKey, String workspaceCode);
 }
