1a31ffaf3c7137641a89f9516246dc05803152db
seungki1011 <kimseungki1011@gmail.com>
2025-02-20 16:31:26 +0900

    Feat: add message for IssueNotFoundException
    - 스프린트 키, 이슈 키, 워크스페이스 코드를 통해 이슈 조회 실패한 경우에 대한 메세지 추가

--- backend/src/main/java/com/tissue/api/issue/exception/IssueNotFoundException.java
+++ backend/src/main/java/com/tissue/api/issue/exception/IssueNotFoundException.java
@@ -4,14 +4,20 @@ import com.tissue.api.common.exception.type.ResourceNotFoundException;
 
 public class IssueNotFoundException extends ResourceNotFoundException {
 
-	private static final String KEY_MESSAGE = "Issue not found with key: %s";
-	private static final String KEY_CODE_MESSAGE = "Issue not found with key %s in workspace %s";
+	private static final String ISSUE_KEY_MESSAGE = "Issue(%s) not found.";
+	private static final String ISSUE_KEY_CODE_MESSAGE = "Issue(%s) not found in workspace(%s).";
+	private static final String ISSUE_KEY_SPRINT_KEY_CODE_MESSAGE = "Issue(%s) not found in sprint(%s)"
+		+ " in workspace(%s).";
 
 	public IssueNotFoundException(String issueKey) {
-		super(String.format(KEY_MESSAGE, issueKey));
+		super(String.format(ISSUE_KEY_MESSAGE, issueKey));
 	}
 
 	public IssueNotFoundException(String issueKey, String workspaceCode) {
-		super(String.format(KEY_CODE_MESSAGE, issueKey, workspaceCode));
+		super(String.format(ISSUE_KEY_CODE_MESSAGE, issueKey, workspaceCode));
+	}
+
+	public IssueNotFoundException(String issueKey, String sprintKey, String workspaceCode) {
+		super(String.format(ISSUE_KEY_SPRINT_KEY_CODE_MESSAGE, issueKey, sprintKey, workspaceCode));
 	}
 }
