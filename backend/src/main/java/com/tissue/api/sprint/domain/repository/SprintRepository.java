4d38acd64c269b2a053fa6118d7e8a2696011ff5
seungki1011 <kimseungki1011@gmail.com>
2025-02-26 10:51:29 +0900

    Feat: modify sprint issue search method to find using keyword

--- backend/src/main/java/com/tissue/api/sprint/domain/repository/SprintRepository.java
+++ backend/src/main/java/com/tissue/api/sprint/domain/repository/SprintRepository.java
@@ -9,8 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.tissue.api.issue.domain.Issue;
+import com.tissue.api.issue.presentation.condition.IssueSearchCondition;
 import com.tissue.api.sprint.domain.Sprint;
-import com.tissue.api.sprint.presentation.condition.SprintIssueSearchCondition;
 
 public interface SprintRepository extends JpaRepository<Sprint, Long> {
 
@@ -33,17 +33,35 @@ public interface SprintRepository extends JpaRepository<Sprint, Long> {
 		@Param("workspaceCode") String workspaceCode
 	);
 
+	// @Query("SELECT si.issue FROM Sprint s "
+	// 	+ "JOIN s.sprintIssues si "
+	// 	+ "WHERE s.sprintKey = :sprintKey "
+	// 	+ "AND s.workspaceCode = :workspaceCode "
+	// 	+ "AND (:#{#condition.statuses.isEmpty()} = true OR si.issue.status IN :#{#condition.statuses}) "
+	// 	+ "AND (:#{#condition.types.isEmpty()} = true OR si.issue.type IN :#{#condition.types}) "
+	// 	+ "AND (:#{#condition.priorities.isEmpty()} = true OR si.issue.priority IN :#{#condition.priorities})")
+	// Page<Issue> findIssuesInSprint(
+	// 	@Param("sprintKey") String sprintKey,
+	// 	@Param("workspaceCode") String workspaceCode,
+	// 	@Param("condition") IssueSearchCondition condition,
+	// 	Pageable pageable
+	// );
+
 	@Query("SELECT si.issue FROM Sprint s "
 		+ "JOIN s.sprintIssues si "
 		+ "WHERE s.sprintKey = :sprintKey "
 		+ "AND s.workspaceCode = :workspaceCode "
 		+ "AND (:#{#condition.statuses.isEmpty()} = true OR si.issue.status IN :#{#condition.statuses}) "
 		+ "AND (:#{#condition.types.isEmpty()} = true OR si.issue.type IN :#{#condition.types}) "
-		+ "AND (:#{#condition.priorities.isEmpty()} = true OR si.issue.priority IN :#{#condition.priorities})")
+		+ "AND (:#{#condition.priorities.isEmpty()} = true OR si.issue.priority IN :#{#condition.priorities}) "
+		+ "AND (:#{#condition.keyword} IS NULL OR "
+		+ "LOWER(si.issue.issueKey) LIKE LOWER(CONCAT('%', :#{#condition.keyword}, '%')) OR "
+		+ "LOWER(si.issue.title) LIKE LOWER(CONCAT('%', :#{#condition.keyword}, '%')) OR "
+		+ "LOWER(si.issue.content) LIKE LOWER(CONCAT('%', :#{#condition.keyword}, '%')))")
 	Page<Issue> findIssuesInSprint(
 		@Param("sprintKey") String sprintKey,
 		@Param("workspaceCode") String workspaceCode,
-		@Param("condition") SprintIssueSearchCondition condition,
+		@Param("condition") IssueSearchCondition condition,
 		Pageable pageable
 	);
 }
