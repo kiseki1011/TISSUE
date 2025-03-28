7ac5b00530ea41e0f7a5dd7572181e595663cab9
seungki1011 <kimseungki1011@gmail.com>
2025-03-28 18:48:41 +0900

    Refactor: add methods for DomainEvent interface

--- backend/src/main/java/com/tissue/api/common/event/DomainEvent.java
+++ backend/src/main/java/com/tissue/api/common/event/DomainEvent.java
@@ -3,11 +3,26 @@ package com.tissue.api.common.event;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.tissue.api.notification.domain.enums.NotificationEntityType;
+import com.tissue.api.notification.domain.enums.NotificationType;
+
 public interface DomainEvent {
 
 	UUID getEventId();
 
 	LocalDateTime getOccurredAt();
+
+	NotificationType getNotificationType();
+
+	NotificationEntityType getEntityType();
+
+	Long getEntityId();
+
+	String getEntityKey();
+
+	String getWorkspaceCode();
+
+	Long getTriggeredByWorkspaceMemberId();
 
 	String getType();
 }
