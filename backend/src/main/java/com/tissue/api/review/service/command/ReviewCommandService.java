64802e9a1a7257cb86282d9d0cb61b054e736025
seungki1011 <kimseungki1011@gmail.com>
2025-04-05 20:30:59 +0900

    Refactor: add reviewer reader service

--- backend/src/main/java/com/tissue/api/review/service/command/ReviewCommandService.java
+++ backend/src/main/java/com/tissue/api/review/service/command/ReviewCommandService.java
@@ -1,14 +1,14 @@
 package com.tissue.api.review.service.command;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.tissue.api.common.exception.type.InvalidOperationException;
 import com.tissue.api.issue.domain.Issue;
 import com.tissue.api.issue.service.command.IssueReader;
 import com.tissue.api.review.domain.IssueReviewer;
 import com.tissue.api.review.domain.Review;
-import com.tissue.api.review.domain.repository.IssueReviewerRepository;
+import com.tissue.api.review.domain.event.ReviewSubmittedEvent;
 import com.tissue.api.review.domain.repository.ReviewRepository;
 import com.tissue.api.review.presentation.dto.request.SubmitReviewRequest;
 import com.tissue.api.review.presentation.dto.request.UpdateReviewRequest;
@@ -27,28 +27,26 @@ import lombok.RequiredArgsConstructor;
 public class ReviewCommandService {
 
 	private final ReviewReader reviewReader;
+	private final ReviewerReader reviewerReader;
 	private final IssueReader issueReader;
 
 	private final ReviewRepository reviewRepository;
-	private final IssueReviewerRepository issueReviewerRepository;
+
+	private final ApplicationEventPublisher eventPublisher;
 
 	@Transactional
 	public SubmitReviewResponse submitReview(
 		String workspaceCode,
 		String issueKey,
-		Long reviewerWorkspaceMemberId,
+		Long currentWorkspaceMemberId,
 		SubmitReviewRequest request
 	) {
 		Issue issue = issueReader.findIssue(issueKey, workspaceCode);
 
-		IssueReviewer issueReviewer = issueReviewerRepository.findByIssueKeyAndReviewerId(
-				issueKey,
-				reviewerWorkspaceMemberId
-			)
-			.orElseThrow(() -> new InvalidOperationException(String.format(
-				"Must be a reviewer to create a review. issueKey: %s, workspaceMemberId: %d",
-				issueKey, reviewerWorkspaceMemberId))
-			);
+		IssueReviewer issueReviewer = reviewerReader.findByIssueKeyAndWorkspaceMemberId(
+			issueKey,
+			currentWorkspaceMemberId
+		);
 
 		issue.validateCanSubmitReview();
 
@@ -61,6 +59,10 @@ public class ReviewCommandService {
 		Review savedReview = reviewRepository.save(review);
 
 		// TODO: ReviewSubmittedEvent(이슈의 구독자)
+		// TODO: ReviewSubmittedEvent로 Review를 넘기는게 좋을까? 아니면 그냥 id를 넘기는게 좋을까?
+		eventPublisher.publishEvent(
+			ReviewSubmittedEvent.createEvent(issue, currentWorkspaceMemberId, savedReview.getId())
+		);
 
 		return SubmitReviewResponse.from(savedReview);
 	}
