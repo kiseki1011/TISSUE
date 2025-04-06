ab1dac4b389f5b6e3cfeedcadf97fee5c96a487e
seungki1011 <kimseungki1011@gmail.com>
2025-04-06 15:31:39 +0900

    Feat: add event handling

--- backend/src/main/java/com/tissue/api/event/NotificationEventHandler.java
+++ backend/src/main/java/com/tissue/api/event/NotificationEventHandler.java
@@ -8,9 +8,15 @@ import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.tissue.api.issue.domain.event.IssueCreatedEvent;
+import com.tissue.api.issue.domain.event.IssueParentAssignedEvent;
+import com.tissue.api.issue.domain.event.IssueParentRemovedEvent;
+import com.tissue.api.issue.domain.event.IssueStatusChangedEvent;
 import com.tissue.api.issue.domain.event.IssueUpdatedEvent;
 import com.tissue.api.notification.domain.NotificationProcessor;
 import com.tissue.api.notification.domain.NotificationTargetResolver;
+import com.tissue.api.review.domain.event.ReviewRequestedEvent;
+import com.tissue.api.review.domain.event.ReviewSubmittedEvent;
+import com.tissue.api.review.domain.event.ReviewerAddedEvent;
 import com.tissue.api.workspacemember.domain.WorkspaceMember;
 
 import lombok.RequiredArgsConstructor;
@@ -30,7 +36,10 @@ public class NotificationEventHandler {
 	@Async("notificationTaskExecutor")
 	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
 	public void handleIssueCreated(IssueCreatedEvent event) {
-		log.debug("Processing issue created event. issue id: {}", event.getIssueId());
+		log.debug(
+			"Processing issue created event. issue key: {}, workspace code: {}",
+			event.getIssueKey(), event.getWorkspaceCode()
+		);
 
 		List<WorkspaceMember> targets = targetResolver.getWorkspaceWideMemberTargets(event.getWorkspaceCode());
 		notificationProcessor.processNotification(event, targets);
@@ -39,10 +48,111 @@ public class NotificationEventHandler {
 	@Async("notificationTaskExecutor")
 	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
 	public void handleIssueUpdated(IssueUpdatedEvent event) {
-		log.debug("Processing issue updated event. issue id: {}", event.getIssueId());
+		log.debug(
+			"Processing issue updated event. issue key: {}, workspace code: {}",
+			event.getIssueKey(), event.getWorkspaceCode()
+		);
 
-		List<WorkspaceMember> targets = targetResolver.getIssueSubscriberTargets(event.getIssueId());
+		List<WorkspaceMember> targets = targetResolver.getIssueSubscriberTargets(
+			event.getIssueKey(),
+			event.getWorkspaceCode()
+		);
 		notificationProcessor.processNotification(event, targets);
 	}
 
+	@Async("notificationTaskExecutor")
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void handleIssueStatusChanged(IssueStatusChangedEvent event) {
+		log.debug(
+			"Processing issue status changed event. issue key: {}, workspace code: {}",
+			event.getIssueKey(), event.getWorkspaceCode()
+		);
+
+		List<WorkspaceMember> targets = targetResolver.getIssueSubscriberTargets(
+			event.getIssueKey(),
+			event.getWorkspaceCode()
+		);
+
+		notificationProcessor.processNotification(event, targets);
+	}
+
+	@Async("notificationTaskExecutor")
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void handleIssueParentAssigned(IssueParentAssignedEvent event) {
+		log.debug(
+			"Processing issue parent assigned event. issue key: {}, workspace code: {}",
+			event.getIssueKey(), event.getWorkspaceCode()
+		);
+
+		List<WorkspaceMember> targets = targetResolver.getIssueSubscriberTargets(
+			event.getIssueKey(),
+			event.getWorkspaceCode()
+		);
+
+		notificationProcessor.processNotification(event, targets);
+	}
+
+	@Async("notificationTaskExecutor")
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void handleIssueParentRemoved(IssueParentRemovedEvent event) {
+		log.debug(
+			"Processing issue parent removed event. issue key: {}, workspace code: {}",
+			event.getIssueKey(), event.getWorkspaceCode()
+		);
+
+		List<WorkspaceMember> targets = targetResolver.getIssueSubscriberTargets(
+			event.getIssueKey(),
+			event.getWorkspaceCode()
+		);
+
+		notificationProcessor.processNotification(event, targets);
+	}
+
+	@Async("notificationTaskExecutor")
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void handleReviewerAdded(ReviewerAddedEvent event) {
+		log.debug(
+			"Processing issue reviewer added event. issue key: {}, workspace code: {}, reviewer wm id: {}",
+			event.getIssueKey(), event.getWorkspaceCode(), event.getReviewerWorkspaceMemberId()
+		);
+
+		List<WorkspaceMember> targets = targetResolver.getIssueSubscriberTargets(
+			event.getIssueKey(),
+			event.getWorkspaceCode()
+		);
+
+		notificationProcessor.processNotification(event, targets);
+	}
+
+	@Async("notificationTaskExecutor")
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void handleReviewRequested(ReviewRequestedEvent event) {
+		log.debug(
+			"Processing issue review requested event. issue key: {}, workspace code: {}",
+			event.getIssueKey(), event.getWorkspaceCode()
+		);
+
+		List<WorkspaceMember> targets = targetResolver.getIssueSubscriberTargets(
+			event.getIssueKey(),
+			event.getWorkspaceCode()
+		);
+
+		notificationProcessor.processNotification(event, targets);
+	}
+
+	@Async("notificationTaskExecutor")
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void handleReviewSubmitted(ReviewSubmittedEvent event) {
+		log.debug(
+			"Processing issue review submitted event. issue key: {}, workspace code: {}, reviewId: {}",
+			event.getIssueKey(), event.getWorkspaceCode(), event.getReviewId()
+		);
+
+		List<WorkspaceMember> targets = targetResolver.getIssueSubscriberTargets(
+			event.getIssueKey(),
+			event.getWorkspaceCode()
+		);
+
+		notificationProcessor.processNotification(event, targets);
+	}
 }
