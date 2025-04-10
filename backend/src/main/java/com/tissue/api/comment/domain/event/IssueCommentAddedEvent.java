6fa87c8350a8bb0279710a1fcc2f50d998dddbbb
seungki1011 <kimseungki1011@gmail.com>
2025-04-11 00:33:00 +0900

    Feat: add comment related events

--- backend/src/main/java/com/tissue/api/comment/domain/event/IssueCommentAddedEvent.java
+++ backend/src/main/java/com/tissue/api/comment/domain/event/IssueCommentAddedEvent.java
@@ -1,4 +1,58 @@
 package com.tissue.api.comment.domain.event;
 
-public class IssueCommentAddedEvent {
+import com.tissue.api.comment.domain.IssueComment;
+import com.tissue.api.issue.domain.Issue;
+import com.tissue.api.issue.domain.enums.IssueType;
+import com.tissue.api.notification.domain.enums.NotificationType;
+import com.tissue.api.notification.domain.enums.ResourceType;
+import com.tissue.api.notification.domain.vo.EntityReference;
+
+import lombok.Getter;
+
+@Getter
+public class IssueCommentAddedEvent extends CommentEvent {
+
+	public IssueCommentAddedEvent(
+		Long issueId,
+		String issueKey,
+		String workspaceCode,
+		IssueType issueType,
+		Long triggeredByWorkspaceMemberId,
+		Long commentId
+	) {
+		super(
+			NotificationType.ISSUE_COMMENT_ADDED,
+			ResourceType.ISSUE,
+			workspaceCode,
+			issueId,
+			issueKey,
+			issueType,
+			triggeredByWorkspaceMemberId,
+			commentId
+		);
+	}
+
+	public static IssueCommentAddedEvent createEvent(
+		Issue issue,
+		IssueComment comment,
+		Long triggeredByWorkspaceMemberId
+	) {
+		return new IssueCommentAddedEvent(
+			issue.getId(),
+			issue.getIssueKey(),
+			issue.getWorkspaceCode(),
+			issue.getType(),
+			triggeredByWorkspaceMemberId,
+			comment.getId()
+		);
+	}
+
+	@Override
+	public EntityReference createEntityReference() {
+		return EntityReference.forIssueComment(
+			getWorkspaceCode(),
+			getIssueKey(),
+			getCommentId()
+		);
+	}
 }
