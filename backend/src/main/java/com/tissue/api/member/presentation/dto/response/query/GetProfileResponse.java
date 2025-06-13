559ba41f4e502892d1c674c266506c616d6b264b
seungki1011 <kimseungki1011@gmail.com>
2025-06-13 21:03:58 +0900

    Refactor: use builder for ProfileResponse

--- backend/src/main/java/com/tissue/api/member/presentation/dto/response/query/GetProfileResponse.java
+++ backend/src/main/java/com/tissue/api/member/presentation/dto/response/query/GetProfileResponse.java
@@ -6,9 +6,13 @@ import java.time.LocalDateTime;
 import com.tissue.api.member.domain.model.Member;
 import com.tissue.api.member.domain.model.enums.JobType;
 
+import lombok.Builder;
+
+@Builder
 public record GetProfileResponse(
 	String loginId,
 	String email,
+	String username,
 
 	String name,
 	LocalDate birthDate,
@@ -16,20 +20,20 @@ public record GetProfileResponse(
 
 	int ownedWorkspaceCount,
 
-	LocalDateTime createdAt,
-	LocalDateTime updatedAt
+	LocalDateTime joinedAt,
+	LocalDateTime lastModifiedAt
 ) {
-
 	public static GetProfileResponse from(Member member) {
-		return new GetProfileResponse(
-			member.getLoginId(),
-			member.getEmail(),
-			member.getName(),
-			member.getBirthDate(),
-			member.getJobType(),
-			member.getMyWorkspaceCount(),
-			member.getCreatedDate(),
-			member.getLastModifiedDate()
-		);
+		return GetProfileResponse.builder()
+			.loginId(member.getLoginId())
+			.email(member.getEmail())
+			.username(member.getUsername())
+			.name(member.getName())
+			.birthDate(member.getBirthDate())
+			.jobType(member.getJobType())
+			.joinedAt(member.getCreatedDate())
+			.lastModifiedAt(member.getLastModifiedDate())
+			.ownedWorkspaceCount(member.getMyWorkspaceCount())
+			.build();
 	}
 }
