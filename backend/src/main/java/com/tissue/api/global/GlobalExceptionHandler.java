424db94d58bbee469427e007e3c13ea372ca0a97
seungki1011 <kimseungki1011@gmail.com>
2025-07-01 02:52:40 +0900

    Refactor: use ResponseEntity

--- backend/src/main/java/com/tissue/api/global/GlobalExceptionHandler.java
+++ backend/src/main/java/com/tissue/api/global/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,15 +18,14 @@ import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.tissue.api.common.dto.ApiResponse;
 import com.tissue.api.common.dto.FieldErrorDto;
 import com.tissue.api.common.exception.TissueException;
+import com.tissue.api.common.exception.type.ExternalServiceException;
+import com.tissue.api.common.exception.type.InternalServerException;
+import com.tissue.api.security.authentication.exception.JwtAuthenticationException;
+import com.tissue.api.security.authentication.exception.JwtCreationException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * Todo
- *  - AOP를 사용한 로깅 적용
- *  - 중복 로직 제거: CommonException을 처리하는 핸들러로 수정
- */
 @Slf4j
 @RequiredArgsConstructor
 @RestControllerAdvice
@@ -33,34 +33,20 @@ public class GlobalExceptionHandler {
 
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(Exception.class)
-	public ApiResponse<Void> unexpectedException(Exception exception) {
-		log.error("Unexpected Exception: ", exception);
+	public ApiResponse<Void> unexpectedException(Exception e) {
+		log.error("Unexpected exception: {}", e.getMessage(), e);
 
-		return ApiResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR,
-			"A unexpected problem has occured",
-			null);
-	}
-
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	@ExceptionHandler(RuntimeException.class)
-	public ApiResponse<Void> unexpectedRuntimeException(RuntimeException exception) {
-		log.error("Unexpected RuntimeException:", exception);
-
-		return ApiResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR,
-			"A unexpected problem has occured",
-			null);
+		return ApiResponse.failWithNoContent(HttpStatus.INTERNAL_SERVER_ERROR, "A unexpected problem has occured.");
 	}
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ApiResponse<List<FieldErrorDto>> handleValidationException(MethodArgumentNotValidException exception) {
-		log.error("Validation Exception: ", exception);
+	public ApiResponse<List<FieldErrorDto>> handleValidationException(MethodArgumentNotValidException e) {
+		log.info("Validation failed: {} ", e.getMessage(), e);
 
-		List<FieldErrorDto> errors = extractFieldErrors(exception.getBindingResult());
+		List<FieldErrorDto> errors = extractFieldErrors(e.getBindingResult());
 
-		return ApiResponse.fail(HttpStatus.BAD_REQUEST,
-			"One or more fields have validation errors",
-			errors);
+		return ApiResponse.fail(HttpStatus.BAD_REQUEST, "One or more fields have failed validation.", errors);
 	}
 
 	private List<FieldErrorDto> extractFieldErrors(BindingResult bindingResult) {
@@ -76,24 +62,82 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(HttpMessageNotReadableException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ApiResponse<Void> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+	public ApiResponse<Void> handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
 		// enum 타입 변환 실패로 인한 예외인 경우
-		if (ex.getCause() instanceof InvalidFormatException) {
-			return ApiResponse.fail(HttpStatus.BAD_REQUEST, "Invalid enum value provided", null);
+		if (e.getCause() instanceof InvalidFormatException) {
+			log.warn("Invalid enum value provided: {} ", e.getMessage(), e);
+			return ApiResponse.failWithNoContent(HttpStatus.BAD_REQUEST, "Invalid enum value provided.");
 		}
 
 		// 그 외의 요청 본문 관련 예외
-		return ApiResponse.fail(HttpStatus.BAD_REQUEST, "Invalid request body", null);
+		log.warn("Invalid request body provided: {} ", e.getMessage(), e);
+		return ApiResponse.failWithNoContent(HttpStatus.BAD_REQUEST, "Invalid request body.");
+	}
+
+	@ExceptionHandler(JwtAuthenticationException.class)
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	public ResponseEntity<ApiResponse<Void>> handleJwtAuth(JwtAuthenticationException e) {
+		log.warn("JWT authentication error: {}", e.getMessage(), e);
+
+		HttpStatus httpStatus = e.getHttpStatus();
+
+		return ResponseEntity
+			.status(httpStatus)
+			.body(ApiResponse.failWithNoContent(httpStatus, e.getMessage()));
+	}
+
+	@ExceptionHandler(JwtCreationException.class)
+	public ResponseEntity<ApiResponse<Void>> handleJwtCreation(JwtCreationException e) {
+		log.error("JWT creation failed: {}", e.getMessage(), e);
+
+		HttpStatus httpStatus = e.getHttpStatus();
+
+		return ResponseEntity
+			.status(httpStatus)
+			.body(ApiResponse.failWithNoContent(httpStatus, e.getMessage()));
+	}
+
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public ResponseEntity<ApiResponse<Void>> handleMissingParam(MissingServletRequestParameterException e) {
+		log.warn("Missing request parameter: {}", e.getParameterName());
+
+		return ResponseEntity
+			.status(HttpStatus.BAD_REQUEST)
+			.body(ApiResponse.failWithNoContent(HttpStatus.BAD_REQUEST, "Missing parameter: " + e.getParameterName()));
+	}
+
+	@ExceptionHandler(InternalServerException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public ResponseEntity<ApiResponse<Void>> handleInternalServerException(InternalServerException e) {
+		log.error("Internal server problem: {}", e.getMessage(), e);
+
+		HttpStatus httpStatus = e.getHttpStatus();
+
+		return ResponseEntity
+			.status(httpStatus)
+			.body(ApiResponse.failWithNoContent(httpStatus, e.getMessage()));
+	}
+
+	@ExceptionHandler(ExternalServiceException.class)
+	public ResponseEntity<ApiResponse<Void>> handleExternalServiceException(ExternalServiceException e) {
+		log.error("External service problem: {}", e.getMessage(), e);
+
+		HttpStatus httpStatus = e.getHttpStatus();
+
+		return ResponseEntity
+			.status(httpStatus)
+			.body(ApiResponse.failWithNoContent(httpStatus, e.getMessage()));
 	}
 
 	@ExceptionHandler(TissueException.class)
-	public ResponseEntity<ApiResponse<Void>> handleTissueException(TissueException exception) {
-		log.error("Tissue Exception: ", exception);
+	public ResponseEntity<ApiResponse<Void>> handleTissueException(TissueException e) {
+		log.info("Tissue exception: {}", e.getMessage(), e);
 
-		String message = exception.getMessage();
+		String message = e.getMessage();
+		HttpStatus httpStatus = e.getHttpStatus();
 
 		return ResponseEntity
-			.status(exception.getHttpStatus())
-			.body(ApiResponse.fail(exception.getHttpStatus(), message, null));
+			.status(httpStatus)
+			.body(ApiResponse.failWithNoContent(httpStatus, message));
 	}
 }
