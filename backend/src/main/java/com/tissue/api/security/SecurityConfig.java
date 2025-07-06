46dbf94853d3d34c5dd2e46ebc1781a2272ece5b
seungki1011 <kimseungki1011@gmail.com>
2025-07-07 01:25:12 +0900

    Feat: add needed beans in security config

--- backend/src/main/java/com/tissue/api/security/SecurityConfig.java
+++ backend/src/main/java/com/tissue/api/security/SecurityConfig.java
@@ -3,14 +3,20 @@ package com.tissue.api.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.tissue.api.security.authentication.ExceptionHandlerFilter;
+import com.tissue.api.security.authentication.MemberUserDetailsService;
 import com.tissue.api.security.authentication.jwt.JwtAuthenticationEntryPoint;
 import com.tissue.api.security.authentication.jwt.JwtAuthenticationFilter;
 import com.tissue.api.security.authorization.ApiAccessDeniedHandler;
@@ -62,5 +68,28 @@ public class SecurityConfig {
 			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider(MemberUserDetailsService userDetailsService) {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(passwordEncoder());
+		return provider;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(
+		HttpSecurity http,
+		DaoAuthenticationProvider provider
+	) throws Exception {
+		return http.getSharedObject(AuthenticationManagerBuilder.class)
+			.authenticationProvider(provider)
+			.build();
 	}
 }
