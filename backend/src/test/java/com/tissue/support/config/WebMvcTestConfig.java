ffd8c300ecb4c01fcace778939ddc397bfe13080
seungki1011 <kimseungki1011@gmail.com>
2025-07-12 03:17:36 +0900

    Test: add MockCurrentMemberArgumentResolver for @CurrentMember mocking

--- backend/src/test/java/com/tissue/support/config/WebMvcTestConfig.java
+++ backend/src/test/java/com/tissue/support/config/WebMvcTestConfig.java
@@ -1,27 +1,30 @@
 package com.tissue.support.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.tissue.support.mock.MockAuthorizationInterceptor;
+import com.tissue.support.mock.MockCurrentMemberArgumentResolver;
 
 @TestConfiguration
 public class WebMvcTestConfig implements WebMvcConfigurer {
 
-	@Value("${test.member.id:1}")
-	private Long memberId;
-
 	@Value("${test.allow.hasSufficientRole:true}")
 	private boolean hasSufficientRole;
-
-	@Value("${test.allow.isLogin:true}")
-	private boolean isLogin;
-
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new MockAuthorizationInterceptor(hasSufficientRole))
 			.order(1);
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(new MockCurrentMemberArgumentResolver());
 	}
 }
