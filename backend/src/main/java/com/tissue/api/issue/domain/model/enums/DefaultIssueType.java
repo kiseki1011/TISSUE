5f4b7e7ed66360f3d4ace1d8b9671f3fe9586be3
seungki1011 <kimseungki1011@gmail.com>
2025-07-20 13:55:05 +0900

    Feat: add enum for default issue type

--- backend/src/main/java/com/tissue/api/issue/domain/model/enums/DefaultIssueType.java
+++ backend/src/main/java/com/tissue/api/issue/domain/model/enums/DefaultIssueType.java
@@ -1,9 +1,18 @@
 package com.tissue.api.issue.domain.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum DefaultIssueType {
-	EPIC,
-	STORY,
-	TASK,
-	BUG,
-	SUB_TASK
+	EPIC("Epic"),
+	STORY("Story"),
+	TASK("Task"),
+	BUG("Bug"),
+	SUB_TASK("Sub-task");
+
+	private final String label;
+
+	DefaultIssueType(String label) {
+		this.label = label;
+	}
 }
