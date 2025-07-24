4acedca92b807488b3f248791408c581118ffdf1
seungki1011 <kimseungki1011@gmail.com>
2025-07-25 00:27:26 +0900

    Feat: add workflow validator

---
+++ backend/src/main/java/com/tissue/api/issue/domain/service/validator/WorkflowValidator.java
@@ -0,0 +1,50 @@
+package com.tissue.api.issue.domain.service.validator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+
+import com.tissue.api.common.exception.type.DuplicateResourceException;
+import com.tissue.api.common.exception.type.InvalidOperationException;
+import com.tissue.api.issue.application.dto.CreateWorkflowCommand;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class WorkflowValidator {
+
+	/**
+	 * TODO
+	 *  - validation if main flow of a WorkflowDefinition is a single straight line
+	 *  - i wonder if i can just encapsulate validation logic inside the entities
+	 *  (WorkflowDefinition, WorkflowStep, WorkflowTransition)
+	 */
+
+	public void validateCommand(CreateWorkflowCommand cmd) {
+		Set<String> stepLabels = new HashSet<>();
+		int initialCount = 0;
+		int finalCount = 0;
+
+		for (CreateWorkflowCommand.StepCommand step : cmd.steps()) {
+			if (!stepLabels.add(step.label())) {
+				throw new DuplicateResourceException("Duplicate step label: " + step.label());
+			}
+			if (step.isInitial()) {
+				initialCount++;
+			}
+
+			if (step.isFinal()) {
+				finalCount++;
+			}
+		}
+
+		if (initialCount != 1) {
+			throw new InvalidOperationException("Workflow must have a single initial step.");
+		}
+		if (finalCount == 0) {
+			throw new InvalidOperationException("Workflow must have at least one final step.");
+		}
+	}
+}
