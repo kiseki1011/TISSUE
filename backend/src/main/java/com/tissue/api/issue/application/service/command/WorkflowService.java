c8489b69eac8514767a9d52790a8842a0d5e3b4a
seungki1011 <kimseungki1011@gmail.com>
2025-07-25 00:27:09 +0900

    Feat: add exception handling logic

--- backend/src/main/java/com/tissue/api/issue/application/service/command/WorkflowService.java
+++ backend/src/main/java/com/tissue/api/issue/application/service/command/WorkflowService.java
@@ -3,13 +3,16 @@ package com.tissue.api.issue.application.service.command;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tissue.api.common.exception.type.DuplicateResourceException;
 import com.tissue.api.issue.application.dto.CreateWorkflowCommand;
 import com.tissue.api.issue.domain.newmodel.WorkflowDefinition;
 import com.tissue.api.issue.domain.newmodel.WorkflowStep;
 import com.tissue.api.issue.domain.newmodel.WorkflowTransition;
+import com.tissue.api.issue.domain.service.validator.WorkflowValidator;
 import com.tissue.api.issue.domain.util.KeyGenerator;
 import com.tissue.api.issue.infrastructure.repository.WorkflowRepository;
 import com.tissue.api.issue.presentation.controller.dto.response.WorkflowResponse;
@@ -18,73 +21,78 @@ import com.tissue.api.workspace.domain.model.Workspace;
 
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class WorkflowService {
 
 	private final WorkspaceReader workspaceReader;
 	private final WorkflowRepository workflowRepository;
+	private final WorkflowValidator workflowValidator;
 	private final EntityManager em;
 
 	@Transactional
 	public WorkflowResponse createWorkflow(CreateWorkflowCommand cmd) {
+
+		workflowValidator.validateCommand(cmd);
 		Workspace workspace = workspaceReader.findWorkspace(cmd.workspaceCode());
 
-		WorkflowDefinition workflow = WorkflowDefinition.builder()
-			.workspace(workspace)
-			.label(cmd.label())
-			.build();
-		workflowRepository.save(workflow);
-		em.flush();
-
-		workflow.setKey(KeyGenerator.generateWorkflowKey(workflow.getId()));
-
-		List<WorkflowStep> steps = new ArrayList<>();
-		for (CreateWorkflowCommand.StepCommand s : cmd.steps()) {
-			WorkflowStep step = WorkflowStep.builder()
-				.workflow(workflow)
-				.label(s.label())
-				.isInitial(s.isInitial())
-				.isFinal(s.isFinal())
+		// TODO: I wonder if I understand right how the persistance in JPA works
+		//  - am i using em.flush() right?
+		//  - how exactly does persisting WorkflowStep, WorkflowTransition with cascade work?
+		//  - i wonder if i can remove the flush() code by using a KeySequence instead of the entity's id
+		try {
+			WorkflowDefinition workflow = WorkflowDefinition.builder()
+				.workspace(workspace)
+				.label(cmd.label())
 				.build();
-			workflow.addStep(step);
-			steps.add(step);
+			workflowRepository.save(workflow);
+			em.flush();
+
+			workflow.setKey(KeyGenerator.generateWorkflowKey(workflow.getId()));
+
+			List<WorkflowStep> stepsBuffer = new ArrayList<>();
+
+			for (CreateWorkflowCommand.StepCommand s : cmd.steps()) {
+				WorkflowStep step = WorkflowStep.builder()
+					.workflow(workflow)
+					.label(s.label())
+					.isInitial(s.isInitial())
+					.isFinal(s.isFinal())
+					.build();
+				workflow.addStep(step);
+				stepsBuffer.add(step);
+			}
+			em.flush();
+
+			stepsBuffer.forEach(step -> step.setKey(KeyGenerator.generateStepKey(step.getId())));
+
+			for (CreateWorkflowCommand.TransitionCommand t : cmd.transitions()) {
+				WorkflowStep sourceStep = stepsBuffer.get(t.sourceIndex());
+				WorkflowStep targetStep = stepsBuffer.get(t.targetIndex());
+
+				WorkflowTransition transition = WorkflowTransition.builder()
+					.workflow(workflow)
+					.isMainFlow(t.isMainFlow())
+					.sourceStep(sourceStep)
+					.targetStep(targetStep)
+					.label(t.label())
+					.build();
+				workflow.addTransition(transition);
+			}
+			em.flush();
+
+			workflow.getTransitions().forEach(t -> t.setKey(KeyGenerator.generateTransitionKey(t.getId())));
+			workflow = workflowRepository.save(workflow);
+			em.flush();
+
+			return WorkflowResponse.from(workflow);
+
+		} catch (DataIntegrityViolationException e) {
+			log.info("Duplicate label.", e);
+			throw new DuplicateResourceException("Duplicate label is not allowed for workflows or steps.", e);
 		}
-		em.flush();
-
-		steps.forEach(step -> step.setKey(KeyGenerator.generateStepKey(step.getId())));
-
-		// TODO: Doesnt using the index of the list have problems?
-		//  For example, lets say the given steps list was this: TODO, IN_PROGRESS, PAUSED, IN_REVIEW, DONE, CANCELLED
-		//  The request format is this: (label, isInitial, isFinal)
-		//  Lets say the main flow is TODO -> IN_PROGRESS -> IN_REVIEW -> DONE
-		//  But the following transitions exist too.
-		//  (Any Step except for DONE) -> CANCELLED, (Any Step except for DONE, CANCELLED) -> PAUSED
-		//  CANCELLED is a final step too.
-		//  From my point of view, using the index of the steps list, for the transitions make it kind of hard to follow.
-		//  In this case, cant i just use the label of the step or step id or step key? Or is it still better to use index of the list?
-
-		for (CreateWorkflowCommand.TransitionCommand t : cmd.transitions()) {
-			WorkflowStep sourceStep = steps.get(t.sourceIndex());
-			WorkflowStep targetStep = steps.get(t.targetIndex());
-
-			WorkflowTransition transition = WorkflowTransition.builder()
-				.workflow(workflow)
-				.isMainFlow(t.isMainFlow())
-				.sourceStep(sourceStep)
-				.targetStep(targetStep)
-				.label(t.label())
-				.build();
-			workflow.addTransition(transition);
-		}
-		em.flush();
-
-		workflow.getTransitions().forEach(t -> t.setKey(KeyGenerator.generateTransitionKey(t.getId())));
-
-		workflow = workflowRepository.save(workflow);
-		em.flush();
-
-		return WorkflowResponse.from(workflow);
 	}
 }
