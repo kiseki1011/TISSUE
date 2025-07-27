d362aa15f5636006034331f368eb496edc33bba3
seungki1011 <kimseungki1011@gmail.com>
2025-07-27 17:35:04 +0900

    Docs: add comment

--- backend/src/main/java/com/tissue/api/issue/presentation/controller/command/IssueTypeController.java
+++ backend/src/main/java/com/tissue/api/issue/presentation/controller/command/IssueTypeController.java
@@ -29,12 +29,12 @@ public class IssueTypeController {
 
 	/**
 	 * TODO
-	 *  - create custom issue type(IssueTypeDefinition)
+	 *  - create custom issue type(IssueTypeDefinition) o
 	 *  - update custom issue type
 	 *  - delete custom issue type
 	 *  - prevent deletion of default system issue types
 	 *  <p>
-	 *  - create custom issue field(IssueFieldDefinition)
+	 *  - create custom issue field(IssueFieldDefinition) o
 	 *  - update custom issue field
 	 *  - delete custom issue field
 	 *  - prevent deletion of default system issue fields
