b5fbe336bb3cdc211720de1eca937c7c8baf73c0
seungki1011 <kimseungki1011@gmail.com>
2025-08-28 03:06:32 +0900

    Feat: add archived field to BaseEntity

--- backend/src/main/java/com/tissue/api/common/entity/BaseEntity.java
+++ backend/src/main/java/com/tissue/api/common/entity/BaseEntity.java
@@ -1,5 +1,7 @@
 package com.tissue.api.common.entity;
 
+import java.time.Instant;
+
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -21,20 +23,25 @@ public abstract class BaseEntity extends BaseDateEntity {
 	@LastModifiedBy
 	private Long lastModifiedBy;
 
-	// @Column(nullable = false)
-	// private boolean deleted = false;
-	//
-	// private LocalDateTime deletedAt;
-	//
-	// public void softDelete() {
-	// 	this.deleted = true;
-	// 	this.deletedAt = LocalDateTime.now();
-	// }
-	//
-	// public void restore() {
-	// 	if (this.deleted) {
-	// 		this.deleted = false;
-	// 		this.deletedAt = null;
-	// 	}
-	// }
+	@Column(nullable = false)
+	private boolean archived = false;
+
+	private Instant archivedAt;
+
+	public void archive() {
+		if (!archived) {
+			this.archived = true;
+			this.archivedAt = Instant.now();
+		}
+	}
+
+	/**
+	 * Disable if there is no restore policy.
+	 */
+	public void restore() {
+		if (archived) {
+			this.archived = false;
+			this.archivedAt = null;
+		}
+	}
 }
