d8b0227819671af25c386c84859133a627e560d3
seungki1011 <kimseungki1011@gmail.com>
2025-09-17 18:48:39 +0900

    Feat: add require not empty

--- backend/src/main/java/com/tissue/api/common/util/DomainPreconditions.java
+++ backend/src/main/java/com/tissue/api/common/util/DomainPreconditions.java
@@ -6,25 +6,30 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DomainPreconditions {
 
-	public static String nullToEmpty(String value) {
-		return value == null ? "" : value;
+	public static String nullToEmpty(String val) {
+		return val == null ? "" : val;
 	}
 
 	public static <T> T requireNotNull(T val, String field) {
 		if (val == null) {
 			throw new RuntimeException(field + " must not be null");
 		}
-
 		return val;
 	}
 
 	public static String requireNotBlank(String val, String field) {
-		String strVal = requireNotNull(val, field);
-
-		if (strVal.isEmpty()) {
+		val = requireNotNull(val, field);
+		if (val.isBlank()) {
 			throw new RuntimeException(field + " must not be blank");
 		}
 
-		return strVal;
+		return val;
+	}
+
+	public static String requireNotEmpty(String val, String field) {
+		if (val.isEmpty()) {
+			throw new IllegalArgumentException(field + " must not be empty");
+		}
+		return val;
 	}
 }
