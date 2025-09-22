77d28416105ea791c3d87243b9d0c70d01116410
seungki1011 <kimseungki1011@gmail.com>
2025-09-23 04:23:34 +0900

    remove unused code

--- backend/src/main/java/com/tissue/api/common/util/DomainPreconditions.java
+++ backend/src/main/java/com/tissue/api/common/util/DomainPreconditions.java
@@ -10,25 +10,16 @@ public class DomainPreconditions {
 		return val == null ? "" : val;
 	}
 
-	public static <T> T requireNotNull(T val, String field) {
-		if (val == null) {
-			throw new RuntimeException(field + " must not be null");
-		}
-		return val;
-	}
-
-	public static String requireNotBlank(String val, String field) {
-		val = requireNotNull(val, field);
+	public static String requireNotBlank(String val) {
 		if (val.isBlank()) {
-			throw new RuntimeException(field + " must not be blank");
+			throw new RuntimeException("must not be blank");
 		}
-
 		return val;
 	}
 
-	public static String requireNotEmpty(String val, String field) {
+	public static String requireNotEmpty(String val) {
 		if (val.isEmpty()) {
-			throw new IllegalArgumentException(field + " must not be empty");
+			throw new IllegalArgumentException("must not be empty");
 		}
 		return val;
 	}
