810f7821ceeecfbf7cf209688a4a171628801a0a
seungki1011 <kimseungki1011@gmail.com>
2025-09-29 22:13:32 +0900

    add single row find method

--- backend/src/main/java/com/tissue/api/issue/workflow/infrastructure/repository/WorkflowStepRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tissue.api.issue.workflow.infrastructure.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tissue.api.issue.workflow.domain.model.WorkflowStatus;
-
-public interface WorkflowStepRepository extends JpaRepository<WorkflowStatus, Long> {
-}
