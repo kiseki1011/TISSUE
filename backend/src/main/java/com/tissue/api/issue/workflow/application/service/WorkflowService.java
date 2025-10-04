df2cc84c498190893cf05d7c42f1224aa2ef0c93
seungki1011 <kimseungki1011@gmail.com>
2025-10-05 02:55:40 +0900

    implement workflow status, transition patch

--- backend/src/main/java/com/tissue/api/issue/workflow/application/service/WorkflowService.java
+++ backend/src/main/java/com/tissue/api/issue/workflow/application/service/WorkflowService.java
@@ -8,10 +8,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tissue.api.common.exception.type.DuplicateResourceException;
+import com.tissue.api.common.util.Patchers;
 import com.tissue.api.issue.workflow.application.dto.CreateWorkflowCommand;
+import com.tissue.api.issue.workflow.application.dto.PatchStatusCommand;
+import com.tissue.api.issue.workflow.application.dto.PatchTransitionCommand;
+import com.tissue.api.issue.workflow.application.dto.PatchWorkflowCommand;
 import com.tissue.api.issue.workflow.application.finder.WorkflowFinder;
 import com.tissue.api.issue.workflow.domain.model.Workflow;
 import com.tissue.api.issue.workflow.domain.model.WorkflowStatus;
+import com.tissue.api.issue.workflow.domain.model.WorkflowTransition;
 import com.tissue.api.issue.workflow.domain.service.WorkflowGraphValidator;
 import com.tissue.api.issue.workflow.domain.service.WorkflowValidator;
 import com.tissue.api.issue.workflow.infrastructure.repository.WorkflowRepository;
@@ -71,58 +76,59 @@ public class WorkflowService {
 		}
 	}
 
-	// @Transactional
-	// public WorkflowResponse patch(PatchWorkflowCommand cmd) {
-	// 	Workspace workspace = workspaceFinder.findWorkspace(cmd.workspaceKey());
-	// 	Workflow workflow = workflowFinder.findWorkflow(workspace, cmd.id());
-	//
-	// 	if (!workflow.getLabel().equals(cmd.label())) {
-	// 		workflowValidator.ensureLabelUnique(workspace, cmd.label());
-	// 		Patchers.apply(cmd.label(), workflow::rename);
-	// 	}
-	//
-	// 	Patchers.apply(cmd.description(), workflow::updateDescription);
-	// 	Patchers.apply(cmd.color(), workflow::updateColor);
-	//
-	// 	return WorkflowResponse.from(workflow);
-	// }
-	//
-	// @Transactional
-	// public WorkflowResponse softDelete(DeleteWorkflowCommand cmd) {
-	// 	Workspace workspace = workspaceFinder.findWorkspace(cmd.workspaceKey());
-	// 	Workflow workflow = workflowFinder.findWorkflow(workspace, cmd.id());
-	//
-	// 	// TODO: Workflow의 softDelete 주석 참고
-	// 	// workflowValidator.ensureDeletable();
-	//
-	// 	workflow.softDelete();
-	//
-	// 	return WorkflowResponse.from(workflow);
-	// }
-	//
-	// @Transactional
-	// public WorkflowResponse patchStatus(PatchStatusCommand cmd) {
-	// 	Workspace workspace = workspaceFinder.findWorkspace(cmd.workspaceKey());
-	// 	Workflow workflow = workflowFinder.findWorkflow(workspace, cmd.workflowId());
-	// 	WorkflowStatus status = workflowFinder.findWorkflowStatus(workflow, cmd.statusId());
-	//
-	// 	Patchers.apply(cmd.label(), l -> workflow.renameStatus(status, l));
-	// 	Patchers.apply(cmd.description(), d -> workflow.updateStatusDescription(status, d));
-	// 	Patchers.apply(cmd.color(), c -> workflow.updateStatusColor(status, c));
-	//
-	// 	return WorkflowResponse.from(workflow);
-	// }
-	//
-	// @Transactional
-	// public WorkflowResponse patchTransition(PatchTransitionCommand cmd) {
-	// 	Workspace workspace = workspaceFinder.findWorkspace(cmd.workspaceKey());
-	// 	Workflow workflow = workflowFinder.findWorkflow(workspace, cmd.workflowId());
-	// 	WorkflowTransition transition = workflowFinder.findWorkflowTransition(workflow, cmd.transitionId());
-	//
-	// 	Patchers.apply(cmd.label(), l -> workflow.renameTransition(transition, l));
-	// 	Patchers.apply(cmd.description(), d -> workflow.updateTransitionDescription(transition, d));
-	// 	Patchers.apply(cmd.color(), c -> workflow.updateTransitionColor(transition, c));
-	//
-	// 	return WorkflowResponse.from(workflow);
-	// }
+	@Transactional
+	public WorkflowResponse patch(PatchWorkflowCommand cmd) {
+		Workspace workspace = workspaceFinder.findWorkspace(cmd.workspaceKey());
+		Workflow workflow = workflowFinder.findWorkflow(workspace, cmd.id());
+
+		Patchers.apply(cmd.label(), newLabel -> {
+			if (!workflow.getLabel().equals(newLabel)) {
+				workflowValidator.ensureLabelUnique(workspace, newLabel);
+				workflow.rename(newLabel);
+			}
+		});
+
+		Patchers.apply(cmd.description(), workflow::updateDescription);
+		Patchers.apply(cmd.color(), workflow::updateColor);
+
+		return WorkflowResponse.from(workflow);
+	}
+
+	@Transactional
+	public WorkflowResponse softDelete(String workspaceKey, Long id) {
+		Workspace workspace = workspaceFinder.findWorkspace(workspaceKey);
+		Workflow workflow = workflowFinder.findWorkflow(workspace, id);
+
+		// TODO: Workflow의 softDelete 주석 참고
+		// workflowValidator.ensureDeletable();
+
+		workflow.softDelete();
+
+		return WorkflowResponse.from(workflow);
+	}
+
+	@Transactional
+	public WorkflowResponse patchStatus(PatchStatusCommand cmd) {
+		Workspace workspace = workspaceFinder.findWorkspace(cmd.workspaceKey());
+		Workflow workflow = workflowFinder.findWorkflow(workspace, cmd.workflowId());
+		WorkflowStatus status = workflowFinder.findWorkflowStatus(workflow, cmd.statusId());
+
+		Patchers.apply(cmd.label(), l -> workflow.renameStatus(status, l));
+		Patchers.apply(cmd.description(), status::updateDescription);
+		Patchers.apply(cmd.color(), status::updateColor);
+
+		return WorkflowResponse.from(workflow);
+	}
+
+	@Transactional
+	public WorkflowResponse patchTransition(PatchTransitionCommand cmd) {
+		Workspace workspace = workspaceFinder.findWorkspace(cmd.workspaceKey());
+		Workflow workflow = workflowFinder.findWorkflow(workspace, cmd.workflowId());
+		WorkflowTransition transition = workflowFinder.findWorkflowTransition(workflow, cmd.transitionId());
+
+		Patchers.apply(cmd.label(), l -> workflow.renameTransition(transition, l));
+		Patchers.apply(cmd.description(), transition::updateDescription);
+
+		return WorkflowResponse.from(workflow);
+	}
 }
