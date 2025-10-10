d05b08754a69ff2ae6a935f3a9f4917715a38149
seungki1011 <kimseungki1011@gmail.com>
2025-10-10 15:40:56 +0900

    remove unused method

--- backend/src/main/java/com/tissue/api/issue/domain/enums/IssueHierarchy.java
+++ backend/src/main/java/com/tissue/api/issue/domain/enums/IssueHierarchy.java
@@ -1,8 +1,10 @@
 package com.tissue.api.issue.domain.enums;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum IssueHierarchy {
 
 	EPIC(1), // highest
@@ -11,16 +13,6 @@ public enum IssueHierarchy {
 	MICROTASK(4); // lowest
 
 	private final int level;
-
-	IssueHierarchy(int level) {
-		this.level = level;
-	}
-
-	// TODO: move this to Issue. from Issue call the isOneLevelAbove() method.
-	//  - i dont think enums should have business logic. they should only provide helper methods.
-	public boolean isInvalidParentFor(IssueHierarchy hierarchy) {
-		return !isOneLevelHigher(hierarchy);
-	}
 
 	public boolean isOneLevelHigher(IssueHierarchy hierarchy) {
 		return this.level == hierarchy.level - 1;
