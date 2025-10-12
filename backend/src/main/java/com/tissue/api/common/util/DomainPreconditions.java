65bcbe2a030d2a4517f8fea46c5c0e174e5c34f3
seungki1011 <kimseungki1011@gmail.com>
2025-10-13 02:54:47 +0900

    remove unused code

--- backend/src/main/java/com/tissue/api/common/util/DomainPreconditions.java
+++
@@ -1,8 +0,0 @@
-package com.tissue.api.common.util;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class DomainPreconditions {
-}
