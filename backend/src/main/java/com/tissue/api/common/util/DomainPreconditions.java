b851dc620b2f8252d6abd0377cc604e27316f24c
seungki1011 <kimseungki1011@gmail.com>
2025-10-18 21:45:20 +0900

    add domain preconditions util

---
+++ backend/src/main/java/com/tissue/api/common/util/DomainPreconditions.java
@@ -0,0 +1,57 @@
+package com.tissue.api.common.util;
+
+import java.time.Instant;
+
+import com.tissue.api.common.exception.type.InvalidOperationException;
+import com.tissue.api.workspace.domain.model.Workspace;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class DomainPreconditions {
+
+	public static void requireFutureOrPresent(
+		@NonNull Instant instant,
+		@NonNull String fieldName
+	) {
+		if (instant.isBefore(Instant.now())) {
+			throw new InvalidOperationException(fieldName + " cannot be in the past");
+		}
+	}
+
+	public static void requireMaxSize(
+		int currentSize,
+		int maxSize,
+		@NonNull String collectionName
+	) {
+		if (currentSize >= maxSize) {
+			throw new InvalidOperationException(
+				"Maximum %s limit reached: %d/%d".formatted(collectionName, currentSize, maxSize)
+			);
+		}
+	}
+
+	public static void requireInRange(
+		@NonNull Integer value,
+		int min,
+		int max,
+		@NonNull String fieldName
+	) {
+		if (value < min || value > max) {
+			throw new InvalidOperationException(
+				"%s must be between %d and %d, but was %d".formatted(fieldName, min, max, value)
+			);
+		}
+	}
+
+	public static void requireSameWorkspace(
+		@NonNull Workspace workspace1,
+		@NonNull Workspace workspace2
+	) {
+		if (!workspace1.equals(workspace2)) {
+			throw new InvalidOperationException("Resources must be from the same workspace");
+		}
+	}
+}
