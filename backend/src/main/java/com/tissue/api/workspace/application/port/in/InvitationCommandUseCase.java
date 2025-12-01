1dc383cf5d6e3b7dc9514c72f20369171eb8a39c
seungki1011 <kimseungki1011@gmail.com>
2025-12-01 22:25:56 +0900

    add invitation use case

---
+++ backend/src/main/java/com/tissue/api/workspace/application/port/in/InvitationCommandUseCase.java
@@ -0,0 +1,10 @@
+package com.tissue.api.workspace.application.port.in;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public interface InvitationCommandUseCase {
+
+	// TODO: accept
+	// TODO: reject
+}
