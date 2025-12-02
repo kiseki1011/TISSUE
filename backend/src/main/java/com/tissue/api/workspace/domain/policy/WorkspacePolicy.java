18d9f4100abd6e7ad674cd449d4378f94158d97d
seungki1011 <kimseungki1011@gmail.com>
2025-12-02 19:18:35 +0900

    add workspace policy

--- backend/src/main/java/com/tissue/api/workspace/domain/policy/WorkspacePolicy.java
+++ backend/src/main/java/com/tissue/api/workspace/domain/policy/WorkspacePolicy.java
@@ -1,14 +1,20 @@
 package com.tissue.api.workspace.domain.policy;
 
-import com.tissue.api.workspace.domain.Workspace;
+import com.tissue.api.workspace.domain.exception.WorkspaceMemberLimitExceededException;
 
-public record WorkspacePolicy(
-	int maxMemberCount
-) {
-	public void ensureWithinMemberLimit(Workspace workspace) {
-		if (workspace.getMemberCount() >= maxMemberCount) {
-			// TODO: WorkspaceMemberLimitException
-			throw new RuntimeException("Maximum number of members reached: %d".formatted(maxMemberCount));
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class WorkspacePolicy {
+
+	// TODO:도메인 계층에 스프링 기술 의존을 해도 괜찮다면 @Value("${workspace.policy.max-members}") 사용 고려
+	private final int maxMembers;
+
+	public void ensureCanAddMember(String workspaceKey, int currentCount) {
+		if (currentCount >= maxMembers) {
+			throw new WorkspaceMemberLimitExceededException(workspaceKey, maxMembers);
 		}
 	}
+
+	// TODO: ensureCanAddProject
 }
