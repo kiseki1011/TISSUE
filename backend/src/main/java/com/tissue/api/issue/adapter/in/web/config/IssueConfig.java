bbd4ffbba332ce665f51457b0e012af3eafc28ef
seungki1011 <kimseungki1011@gmail.com>
2025-12-04 23:52:01 +0900

    fix inject value path

--- backend/src/main/java/com/tissue/api/issue/adapter/in/web/config/IssueConfig.java
+++ backend/src/main/java/com/tissue/api/issue/adapter/in/web/config/IssueConfig.java
@@ -8,19 +8,19 @@ import org.springframework.context.annotation.Configuration;
 
 import com.tissue.api.issue.domain.policy.FieldValuePolicy;
 
-// TODO: Consider using @ConfigurationProperties
+// TODO: @ConfigurationProperties 고려
 @Configuration
 public class IssueConfig {
 
-	@Value("${tissue.issue.max-reviewers}")
+	@Value("${tissue.issue.policy.max-reviewers}")
 	private int maxReviewers;
 
 	@Bean
 	public FieldValuePolicy fieldValuePolicy(
-		@Value("${tissue.issue.field.decimal.scale:6}") int decimalScale,
-		@Value("${tissue.issue.field.decimal.rounding:HALF_UP}") RoundingMode roundingMode,
-		@Value("${tissue.issue.field.decimal.digits.integer:14}") int maxIntegerDigits,
-		@Value("${tissue.issue.field.decimal.digits.fraction:6}") int maxFractionDigits
+		@Value("${tissue.issue.policy.field.decimal.scale:6}") int decimalScale,
+		@Value("${tissue.issue.policy.field.decimal.rounding:HALF_UP}") RoundingMode roundingMode,
+		@Value("${tissue.issue.policy.field.decimal.digits.integer:14}") int maxIntegerDigits,
+		@Value("${tissue.issue.policy.field.decimal.digits.fraction:6}") int maxFractionDigits
 	) {
 		return new FieldValuePolicy(
 			decimalScale,
