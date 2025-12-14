2ea61133278bf1f20894c2d67d65cdda947c2164
seungki1011 <kimseungki1011@gmail.com>
2025-12-15 03:07:13 +0900

    add util to replace ternary operator for null check

---
+++ backend/src/main/java/com/tissue/api/common/util/NullSafe.java
@@ -0,0 +1,13 @@
+package com.tissue.api.common.util;
+
+import java.util.function.Function;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class NullSafe {
+	public static <T, R> R get(T target, Function<T, R> mapper) {
+		return target != null ? mapper.apply(target) : null;
+	}
+}
