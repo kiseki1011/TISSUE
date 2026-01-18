dfaa417d0a8c5986a2d6e8527da6fbc9c925743d
seungki1011 <kimseungki1011@gmail.com>
2026-01-18 18:23:53 +0900

    add findAll

--- backend/src/main/java/com/tissue/activitylog/application/port/out/ActivityLogRepository.java
+++ backend/src/main/java/com/tissue/activitylog/application/port/out/ActivityLogRepository.java
@@ -1,9 +1,12 @@
 package com.tissue.activitylog.application.port.out;
 
 import com.tissue.activitylog.domain.ActivityLog;
+import java.util.List;
 import org.springframework.data.repository.Repository;
 
 public interface ActivityLogRepository extends Repository<ActivityLog, Long> {
 
     ActivityLog save(ActivityLog activityLog);
+
+    List<ActivityLog> findAll();
 }
