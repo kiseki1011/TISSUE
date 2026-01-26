af3227e0d3a381c939d23b7a02d25e79b164f303
seungki1011 <kimseungki1011@gmail.com>
2026-01-26 16:53:42 +0900

    fix fields

--- backend/src/main/java/com/tissue/member/adapter/in/web/dto/request/SignupMemberRequest.java
+++ backend/src/main/java/com/tissue/member/adapter/in/web/dto/request/SignupMemberRequest.java
@@ -1,8 +1,5 @@
 package com.tissue.member.adapter.in.web.dto.request;
 
-import com.tissue.common.validator.annotation.pattern.NamePattern;
-import com.tissue.common.validator.annotation.pattern.PasswordPattern;
-import com.tissue.common.validator.annotation.pattern.UsernamePattern;
 import com.tissue.member.application.dto.request.SignupMemberCommand;
 import com.tissue.member.domain.AuthProvider;
 import jakarta.validation.constraints.Email;
@@ -13,9 +10,9 @@ import lombok.Builder;
 @Builder
 public record SignupMemberRequest(
         @NotBlank @Email @Size(min = 4, max = 255) String email,
-        @NotBlank @UsernamePattern @Size(min = 4, max = 32) String username,
-        @NotBlank @PasswordPattern @Size(min = 8, max = 100) String password,
-        @NotBlank @NamePattern @Size(min = 2, max = 50) String name) {
+        @NotBlank @Size(min = 4, max = 32) String username,
+        @NotBlank @Size(min = 4, max = 100) String password,
+        @NotBlank @Size(min = 2, max = 50) String name) {
 
     public SignupMemberCommand toCommand() {
         return SignupMemberCommand.builder()
