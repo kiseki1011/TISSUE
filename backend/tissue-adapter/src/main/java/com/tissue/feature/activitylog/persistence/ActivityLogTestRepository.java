3ea6e6d429eccf3de1d05f3f536dbf2bde100e89
seungki1011 <kimseungki1011@gmail.com>
2026-02-23 01:09:56 +0900

    add activity log specification adapter

--- backend/tissue-adapter/src/main/java/com/tissue/feature/activitylog/persistence/ActivityLogTestRepository.java
+++ backend/tissue-adapter/src/main/java/com/tissue/feature/activitylog/persistence/ActivityLogTestRepository.java
@@ -3,5 +3,4 @@ package com.tissue.feature.activitylog.persistence;
 import com.tissue.feature.activitylog.domain.ActivityLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ActivityLogTestRepository extends JpaRepository<ActivityLog, Long> {
-}
+public interface ActivityLogTestRepository extends JpaRepository<ActivityLog, Long> {}
