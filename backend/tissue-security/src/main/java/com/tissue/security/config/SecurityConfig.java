2880f9f75b4f51deb4c540c6b946564b97b49ef4
seungki1011 <kimseungki1011@gmail.com>
2026-04-07 11:45:54 +0900

    remove swagger-ui

--- backend/tissue-security/src/main/java/com/tissue/security/config/SecurityConfig.java
+++ backend/tissue-security/src/main/java/com/tissue/security/config/SecurityConfig.java
@@ -126,8 +126,7 @@ public class SecurityConfig {
                         .permitAll()
                         .requestMatchers("/login/**", "/oauth2/**")
                         .permitAll()
-                        .requestMatchers(
-                                "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/apidocs", "/*.svg", "/*.png")
+                        .requestMatchers("/v3/api-docs/**", "/apidocs", "/*.svg", "/*.png")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
