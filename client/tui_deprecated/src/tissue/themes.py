e448e5aade27b702c7695ce8a1630846eb589a1d
seungki1011 <kimseungki1011@gmail.com>
2026-05-21 23:01:16 +0900

    remove deprecated tui code

--- client/tui_deprecated/src/tissue/themes.py
+++
@@ -1,9 +0,0 @@
-from textual.app import App
-from textual.theme import Theme
-
-CUSTOM_THEMES: list[Theme] = []
-
-
-def register_custom_themes(app: App) -> None:
-    for theme in CUSTOM_THEMES:
-        app.register_theme(theme)
